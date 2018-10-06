254894d045526725d753e5d05e5a4f0b9672217d
Marko Vidoni <marko.vidoni@gmail.com>
2018-10-07 01:27:46 +0200

    NLP basic text vectorization implemented

---
+++ AIToolbox/NLP/DataPrep/core.py
@@ -0,0 +1,117 @@
+import re
+from nltk.tokenize import word_tokenize
+
+
+def find_sub_list(sl, l):
+    """Find starting and ending position of a sublist in a longer list.
+
+
+    Args:
+        sl (list): sublist
+        l (list): main longer list
+
+    Returns:
+        (int, int): start and end index in the list l. Returns None if sublist is not found in the main list.
+
+    """
+    sll = len(sl)
+    for ind in (i for i, e in enumerate(l) if e == sl[0]):
+        if l[ind:ind+sll] == sl:
+            return ind, ind+sll-1
+
+
+def basic_tokenize(text, use_word_tokenize=True, rm_non_alphanum=True, start_label='START', end_label='END'):
+    """
+
+    Args:
+        text:
+        use_word_tokenize:
+        rm_non_alphanum:
+        start_label:
+        end_label:
+
+    Returns:
+
+    """
+    if rm_non_alphanum:
+        text = re.sub(r'\([^)]*\)', '', text)
+        text = text.replace('-', ' ')
+        text = re.sub(r'[^0-9a-zA-Z. ]', '', text)
+
+    if use_word_tokenize:
+        text_token = [el.lower() for el in word_tokenize(text)]
+    else:
+        text_token = text.lower().split()
+
+    if start_label is not None:
+        text_token = [start_label] + text_token
+    if end_label is not None:
+        text_token = text_token + [end_label]
+
+    return text_token
+
+
+def prepare_vocab_mapping(vocab, padding=True, special_labels=('<OOV>',)):
+    """
+
+    Args:
+        vocab:
+        padding:
+        special_labels:
+
+    Returns:
+
+    """
+    vocab = set(vocab)
+    vocab = sorted(vocab)
+
+    idx_start = padding + len(special_labels)
+
+    word2idx = dict((c, i + idx_start) for i, c in enumerate(vocab))
+    for i, l in enumerate(special_labels, start=padding):
+        word2idx[l] = i
+
+    vocab_size = len(word2idx) + padding
+
+    return word2idx, vocab_size
+
+
+def vectorize_one_text(text_list, word_idx):
+    """
+
+    Args:
+        text_list:
+        word_idx:
+
+    Returns:
+
+    """
+    return [word_idx[w] if w in word_idx else word_idx['<OOV>'] for w in text_list]
+
+
+def vectorize_text(texts_list, word_idx, text_maxlen=None, shorten_text_mode='end'):
+    """
+
+    Args:
+        texts_list:
+        word_idx:
+        text_maxlen:
+        shorten_text_mode:
+
+    Returns:
+
+    """
+    if text_maxlen is not None and text_maxlen > 0:
+        if shorten_text_mode == 'end':
+            text_idx_list = [vectorize_one_text(text_list[:text_maxlen], word_idx) for text_list in texts_list]
+        elif shorten_text_mode == 'start':
+            text_idx_list = [vectorize_one_text(text_list[len(text_list)-text_maxlen:], word_idx)
+                             if len(text_list) > text_maxlen else
+                             vectorize_one_text(text_list, word_idx)
+                             for text_list in texts_list]
+        else:
+            raise ValueError('shorten_text_mode not end or start')
+    else:
+        text_idx_list = [vectorize_one_text(text_list, word_idx) for text_list in texts_list]
+
+    return text_idx_list
--- AIToolbox/NLP/Dataset/SQuAD2DS.py
+++ AIToolbox/NLP/Dataset/SQuAD2DS.py
@@ -1,54 +1,62 @@
-import re
-from nltk.tokenize import word_tokenize
-
-
-"""
-            Not my code, so be careful
-            
-            Got this from github project
-"""
-
-
-def find_sub_list(sl, l):
-    sll = len(sl)
-    for ind in (i for i, e in enumerate(l) if e == sl[0]):
-        if l[ind:ind+sll] == sl:
-            return ind, ind+sll-1
+from AIToolbox.NLP.DataPrep.core import *
 
 
 def process_context_text(context_text, use_word_tokenize=True, rm_non_alphanum=True):
-    if rm_non_alphanum:
-        context_text = re.sub(r'\([^)]*\)', '', context_text)
-        context_text = context_text.replace('-', ' ')
-        context_text = re.sub(r'[^0-9a-zA-Z. ]', '', context_text)
+    """
 
-    if use_word_tokenize:
-        main_text_token = ['START_DOC'] + [el.lower() for el in word_tokenize(context_text)] + ['END_DOC']
-    else:
-        main_text_token = ['START_DOC'] + context_text.lower().split() + ['END_DOC']
+    Args:
+        context_text:
+        use_word_tokenize:
+        rm_non_alphanum:
 
-    return main_text_token
+    Returns:
+
+    """
+    return basic_tokenize(context_text, use_word_tokenize, rm_non_alphanum,
+                          start_label='START_DOC', end_label='END_DOC')
 
 
 def process_question_text(question_text, use_word_tokenize=True, rm_non_alphanum=True):
-    if use_word_tokenize:
-        question_text = ['START_Q'] + [el.lower() for el in word_tokenize(question_text)] + ['END_Q']
-    else:
-        question_text = ['START_Q'] + question_text.lower().split() + ['END_Q']
+    """
 
-    return question_text
+    Args:
+        question_text:
+        use_word_tokenize:
+        rm_non_alphanum:
+
+    Returns:
+
+    """
+    return basic_tokenize(question_text, use_word_tokenize, rm_non_alphanum,
+                          start_label='START_Q', end_label='END_Q')
 
 
 def process_answer_text(answer_text, use_word_tokenize=True, rm_non_alphanum=True):
-    if use_word_tokenize:
-        answer_text = ['START_ANSW'] + [el.lower() for el in word_tokenize(answer_text)] + ['END_ANSW']
-    else:
-        answer_text = ['START_ANSW'] + answer_text.lower().split() + ['END_ANSW']
+    """
 
-    return answer_text
+    Args:
+        answer_text:
+        use_word_tokenize:
+        rm_non_alphanum:
+
+    Returns:
+
+    """
+    return basic_tokenize(answer_text, use_word_tokenize, rm_non_alphanum,
+                          start_label='START_ANSW', end_label='END_ANSW')
 
 
 def build_dataset(data_json, use_word_tokenize=True, rm_non_alphanum=True):
+    """
+
+    Args:
+        data_json:
+        use_word_tokenize:
+        rm_non_alphanum:
+
+    Returns:
+
+    """
     print('Building datasets')
 
     context_text_list = []
@@ -110,27 +118,3 @@ def build_dataset(data_json, use_word_tokenize=True, rm_non_alphanum=True):
            vocab_context_text, vocab_question_text, vocab_answer_text, \
            max_context_text_len, max_question_text_len
 
-
-def prepare_vocab_mapping(vocab):
-    vocab = set(vocab)
-    vocab = sorted(vocab)
-
-    word2idx = dict((c, i + 2) for i, c in enumerate(vocab))
-    word2idx['<OOV>'] = 1
-
-    vocab_size = len(word2idx) + 1
-
-    return word2idx, vocab_size
-
-
-def vectorize_one_text(text_list, word_idx):
-    return [word_idx[w] if w in word_idx else word_idx['<OOV>'] for w in text_list]
-
-
-def vectorize_text(texts_list, word_idx, text_maxlen=None):
-    text_idx_list = [vectorize_one_text(text_list, word_idx) for text_list in texts_list]
-
-    if text_maxlen is not None and text_maxlen > 0:
-        raise ValueError( )
-
-    return text_idx_list
