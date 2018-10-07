eacd10a17ae4c1bf353157e73a7ff628d7b54fdd
Marko Vidoni <marko.vidoni@gmail.com>
2018-10-07 02:16:33 +0200

    New Version 0.0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='AIToolbox',
-      version='0.0.2',
+      version='0.0.3',
       author='Marko Vidoni',
       author_email='marko.viwa@gmail.com',
       url='https://github.com/mv1388/AIToolbox',
