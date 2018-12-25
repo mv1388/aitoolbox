2c0ded5c45cd96e7002218e9c3d0e6772d37de52
Marko Vidoni <marko.vidoni@gmail.com>
2018-12-25 13:57:00 +0100

    unittsts new file

---
+++ tests/test_NLP/test_dataset/test_SQuAD2.py
@@ -0,0 +1,10 @@
+import unittest
+from AIToolbox.NLP.dataset.SQuAD2.SQuAD2DataReader import *
+
+
+# class TestCore_find_sub_list(unittest.TestCase):
+#     def test_find_sub_list_do_find(self):
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
