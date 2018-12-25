ec6f17ec3fc9973cc9c0b731fe0f6fc6ecc6df65
Marko Vidoni <marko.vidoni@gmail.com>
2018-12-25 02:15:01 +0100

    QA span classification result package

--- AIToolbox/NLP/Evaluation/NLPResultPackage.py
+++ AIToolbox/NLP/Evaluation/NLPResultPackage.py
@@ -1,18 +1,20 @@
 from AIToolbox.ExperimentSave.ResultPackage import AbstractResultPackage
+from AIToolbox.ExperimentSave.MetricsGeneral.Classification import AccuracyMetric
 from AIToolbox.NLP.Evaluation.NLPMetrics import BLEUScoreMetric, PerplexityMetric, ROGUEMetric
 
 
 class QuestionAnswerResultPackage(AbstractResultPackage):
-    def __init__(self, y_true, y_predicted, hyperparameters=None, strict_content_check=False):
+    def __init__(self, y_true, y_predicted, hyperparameters=None, training_history=None, strict_content_check=False):
         """
 
         Args:
             y_true (numpy.array or list):
             y_predicted (numpy.array or list):
             hyperparameters (dict):
+            training_history (AIToolbox.ExperimentSave.TrainingHistory.AbstractTrainingHistory):
             strict_content_check (bool):
         """
-        AbstractResultPackage.__init__(self, y_true, y_predicted, hyperparameters, strict_content_check)
+        AbstractResultPackage.__init__(self, y_true, y_predicted, hyperparameters, training_history, strict_content_check)
 
     def prepare_results_dict(self):
         """
@@ -23,17 +25,59 @@ class QuestionAnswerResultPackage(AbstractResultPackage):
         raise NotImplementedError
 
 
+class QuestionAnswerSpanClassificationResultPackage(AbstractResultPackage):
+    def __init__(self,
+                 y_span_start_true, y_span_start_predicted,
+                 y_span_end_true, y_span_end_predicted,
+                 hyperparameters=None, training_history=None, strict_content_check=False):
+        """
+
+        Args:
+            y_span_start_true (numpy.array or list):
+            y_span_start_predicted (numpy.array or list):
+            y_span_end_true (numpy.array or list):
+            y_span_end_predicted (numpy.array or list):
+            hyperparameters (dict):
+            training_history (AIToolbox.ExperimentSave.TrainingHistory.AbstractTrainingHistory):
+            strict_content_check (bool):
+        """
+        self.y_span_start_true = y_span_start_true
+        self.y_span_end_true = y_span_end_true
+
+        self.y_span_start_predicted = y_span_start_predicted
+        self.y_span_end_predicted = y_span_end_predicted
+
+        AbstractResultPackage.__init__(self, None, None, hyperparameters, training_history, strict_content_check)
+
+    def prepare_results_dict(self):
+        """
+
+        Returns:
+
+        """
+        span_start_accuracy = AccuracyMetric(self.y_span_start_true, self.y_span_start_predicted)
+        span_start_accuracy.metric_name += '_span_start'
+        span_end_accuracy = AccuracyMetric(self.y_span_end_true, self.y_span_end_predicted)
+        span_end_accuracy.metric_name += '_span_end'
+
+        span_start_accuracy_result = span_start_accuracy.get_metric_dict()
+        span_end_accuracy_result = span_end_accuracy.get_metric_dict()
+
+        self.results_dict = {**span_start_accuracy_result, **span_end_accuracy_result}
+
+
 class TextSummarizationResultPackage(AbstractResultPackage):
-    def __init__(self, y_true, y_predicted, hyperparameters=None, strict_content_check=False):
+    def __init__(self, y_true, y_predicted, hyperparameters=None, training_history=None, strict_content_check=False):
         """
 
         Args:
             y_true (numpy.array or list):
             y_predicted (numpy.array or list):
             hyperparameters (dict):
+            training_history (AIToolbox.ExperimentSave.TrainingHistory.AbstractTrainingHistory):
             strict_content_check (bool):
         """
-        AbstractResultPackage.__init__(self, y_true, y_predicted, hyperparameters, strict_content_check)
+        AbstractResultPackage.__init__(self, y_true, y_predicted, hyperparameters, training_history, strict_content_check)
 
     def prepare_results_dict(self):
         """
@@ -47,16 +91,17 @@ class TextSummarizationResultPackage(AbstractResultPackage):
 
 
 class MachineTranslationResultPackage(AbstractResultPackage):
-    def __init__(self, y_true, y_predicted, hyperparameters=None, strict_content_check=False):
+    def __init__(self, y_true, y_predicted, hyperparameters=None, training_history=None, strict_content_check=False):
         """
 
         Args:
             y_true (numpy.array or list):
             y_predicted (numpy.array or list):
             hyperparameters (dict):
+            training_history (AIToolbox.ExperimentSave.TrainingHistory.AbstractTrainingHistory):
             strict_content_check (bool):
         """
-        AbstractResultPackage.__init__(self, y_true, y_predicted, hyperparameters, strict_content_check)
+        AbstractResultPackage.__init__(self, y_true, y_predicted, hyperparameters, training_history, strict_content_check)
 
     def prepare_results_dict(self):
         """
