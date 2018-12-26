c4ed88dfb034f2167c67541531813a37c38b31e5
Marko Vidoni <marko.vidoni@gmail.com>
2018-12-26 16:15:54 +0100

    batch model feed def class

--- AIToolbox/torchtrain/batch_model_feed_defs.py
+++ AIToolbox/torchtrain/batch_model_feed_defs.py
@@ -1,24 +1,45 @@
+from abc import ABC, abstractmethod
+
 
 """
-    Functions defining the handling of a single batch and feeding it into the PyTorch model
+    Class / Functions defining the handling of a single batch and feeding it into the PyTorch model
 
     Such a function is supplied as an argument to the main train loop code
 """
 
 
-def qa_span_squad_batch_model_feed(model, batch_data, criterion, device):
-    paragraph_batch, paragraph_lengths, question_batch, question_lengths, span = batch_data
+class AbstractModelFeedDefinition(ABC):
+    @abstractmethod
+    def get_loss(self, model, batch_data, criterion, device):
+        pass
 
-    paragraph_batch = paragraph_batch.to(device)
-    paragraph_lengths = paragraph_lengths.to(device)
-    question_batch = question_batch.to(device)
-    question_lengths = question_lengths.to(device)
-    span = span.to(device)
+    @abstractmethod
+    def get_predictions(self, model, batch_data, device):
+        pass
 
-    output_start_span, output_end_span = model(paragraph_batch, question_batch, paragraph_lengths, question_lengths)
 
-    loss1 = criterion(output_start_span, span[:, 0].long())
-    loss2 = criterion(output_end_span, span[:, 1].long())
-    loss = loss1 + loss2
+class QASpanSQuADModelFeedDefinition(AbstractModelFeedDefinition):
+    # def __init__(self):
+    #     self.feed_type = ''
 
-    return loss
+    def get_loss(self, model, batch_data, criterion, device):
+        paragraph_batch, paragraph_lengths, question_batch, question_lengths, span = batch_data
+
+        paragraph_batch = paragraph_batch.to(device)
+        paragraph_lengths = paragraph_lengths.to(device)
+        question_batch = question_batch.to(device)
+        question_lengths = question_lengths.to(device)
+        span = span.to(device)
+
+        output_start_span, output_end_span = model(paragraph_batch, question_batch, paragraph_lengths, question_lengths)
+
+        loss1 = criterion(output_start_span, span[:, 0].long())
+        loss2 = criterion(output_end_span, span[:, 1].long())
+        loss = loss1 + loss2
+
+        return loss
+
+    def get_predictions(self, model, batch_data, device):
+        raise NotImplementedError
+
+        return y_test, y_pred
