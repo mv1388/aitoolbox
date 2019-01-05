cb08e338599783d29e6b22d244f234d636348ae5
Marko Vidoni <marko.vidoni@gmail.com>
2019-01-05 19:49:21 +0100

    Heatmap initial dev

--- AIToolbox/NLP/evaluation/attention_heatmap.py
+++ AIToolbox/NLP/evaluation/attention_heatmap.py
@@ -1,17 +1,20 @@
+import seaborn as sns
+
 from AIToolbox.experiment_save.core_metrics.base_metric import AbstractBaseMetric
 
 
 class AttentionHeatMap(AbstractBaseMetric):
-    def __init__(self, attention_matrix, attention_sent):
+    def __init__(self, attention_matrix, attention_sent_pair):
         """
 
         Args:
             attention_matrix (numpy.array):
-            attention_sent (str):
+            attention_sent_pair (list):
         """
         self.attention_matrix = attention_matrix
-
         AbstractBaseMetric.__init__(self, None, None)
+
+        attention_sent = attention_sent_pair[0]
         self.metric_name = 'AttentionHeatMap_' + attention_sent if len(attention_sent) < 20 else attention_sent[:20]
 
     def calculate_metric(self):
@@ -19,15 +22,15 @@ class AttentionHeatMap(AbstractBaseMetric):
 
 
 class AttentionMultipleHeatMaps(AttentionHeatMap):
-    def __init__(self, attention_matrices, attention_sents):
+    def __init__(self, attention_matrices, attention_sent_pairs):
         """
 
         Args:
             attention_matrices (numpy.array):
-            attention_sents (list):
+            attention_sent_pairs (list):
         """
         self.attention_matrices = attention_matrices
-        self.attention_sents = attention_sents
+        self.attention_sent_pairs = attention_sent_pairs
 
         AbstractBaseMetric.__init__(self, None, None)
         self.metric_name = 'AttentionMultipleHeatMaps'
