19bc344bc860af9a3e1937ac9d113226d73b4e1f
Marko Vidoni <marko.vidoni@gmail.com>
2019-02-01 08:27:02 +0100

    GeneralLRScheduler structure

--- AIToolbox/torchtrain/callbacks/train_schedule_callbacks.py
+++ AIToolbox/torchtrain/callbacks/train_schedule_callbacks.py
@@ -15,7 +15,78 @@ class BasicLearnRateScheduler(AbstractCallback):
         pass
 
 
-class ReduceLROnPlateauScheduler(AbstractCallback):
+# class ReduceLROnPlateauScheduler(AbstractCallback):
+#     def __init__(self, **kwargs):
+#         """
+#
+#         def __init__(self, mode='min', factor=0.1, patience=10,
+#                  verbose=False, threshold=1e-4, threshold_mode='rel', cooldown=0, min_lr=0, eps=1e-8):
+#
+#         Args:
+#             mode:
+#             factor:
+#             patience:
+#             verbose:
+#             threshold:
+#             threshold_mode:
+#             cooldown:
+#             min_lr:
+#             eps:
+#         """
+#         AbstractCallback.__init__(self, 'Reduce learn rate if the model hits the plateau')
+#         self.scheduler_args = kwargs
+#         self.scheduler = None
+#
+#     def register_train_loop_obj(self, train_loop_obj):
+#         self.train_loop_obj = train_loop_obj
+#         self.scheduler = ReduceLROnPlateau(self.train_loop_obj.optimizer, **self.scheduler_args)
+#         return self
+#
+#     def on_epoch_end(self):
+#         val_loss_avg = self.train_loop_obj.evaluate_loss_on_validation()
+#         self.scheduler.step(val_loss_avg)
+
+
+# class GeneralLRScheduler(ReduceLROnPlateauScheduler):
+#     def __init__(self, scheduler_class, **kwargs):
+#         """
+#
+#         Args:
+#             scheduler_class:
+#             **kwargs:
+#         """
+#         ReduceLROnPlateauScheduler.__init__(self, **kwargs)
+#         self.scheduler_class = scheduler_class
+#
+#     def register_train_loop_obj(self, train_loop_obj):
+#         self.train_loop_obj = train_loop_obj
+#         self.scheduler = self.scheduler_class(self.train_loop_obj.optimizer, **self.scheduler_args)
+#         return self
+
+
+class GeneralLRScheduler(AbstractCallback):
+    def __init__(self, scheduler_class, **kwargs):
+        """
+
+        Args:
+            **kwargs:
+        """
+        AbstractCallback.__init__(self, 'General learn rate scheduler')
+        self.scheduler_args = kwargs
+        self.scheduler_class = scheduler_class
+        self.scheduler = None
+
+    def register_train_loop_obj(self, train_loop_obj):
+        self.train_loop_obj = train_loop_obj
+        self.scheduler = self.scheduler_class(self.train_loop_obj.optimizer, **self.scheduler_args)
+        return self
+
+    def on_epoch_end(self):
+        val_loss_avg = self.train_loop_obj.evaluate_loss_on_validation()
+        self.scheduler.step(val_loss_avg)
+
+
+class ReduceLROnPlateauScheduler(GeneralLRScheduler):
     def __init__(self, **kwargs):
         """
 
@@ -33,15 +104,5 @@ class ReduceLROnPlateauScheduler(AbstractCallback):
             min_lr:
             eps:
         """
-        AbstractCallback.__init__(self, 'Reduce learn rate if the model hits the plateau')
-        self.scheduler_args = kwargs
-        self.scheduler = None
-
-    def register_train_loop_obj(self, train_loop_obj):
-        self.train_loop_obj = train_loop_obj
-        self.scheduler = ReduceLROnPlateau(self.train_loop_obj.optimizer, **self.scheduler_args)
-        return self
-
-    def on_epoch_end(self):
-        val_loss_avg = self.train_loop_obj.evaluate_loss_on_validation()
-        self.scheduler.step(val_loss_avg)
+        GeneralLRScheduler.__init__(self, ReduceLROnPlateau, **kwargs)
+        self.callback_name = 'Reduce learn rate if the model hits the plateau'
