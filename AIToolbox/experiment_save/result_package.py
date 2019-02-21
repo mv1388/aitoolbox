35258d365ce8b0ef175d1166c3534f36b176d8ee
Marko Vidoni <marko.vidoni@gmail.com>
2019-02-21 22:01:01 +0100

    Training History refactoring - cleaning

--- AIToolbox/experiment_save/result_package.py
+++ AIToolbox/experiment_save/result_package.py
@@ -38,7 +38,7 @@ class AbstractResultPackage(ABC):
             y_true (numpy.array or list):
             y_predicted (numpy.array or list):
             hyperparameters (dict or None):
-            training_history (AIToolbox.ExperimentSave.training_history.AbstractTrainingHistory):
+            training_history (AIToolbox.ExperimentSave.training_history.TrainingHistory):
             **kwargs (dict):
 
         Returns:
