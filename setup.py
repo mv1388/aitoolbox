deaa7c9e5b7bcac70ea2979132fe2a6826d30b2e
Marko Vidoni <marko.vidoni@gmail.com>
2019-03-10 11:51:54 +0100

    rm email

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(name='AIToolbox',
       version='0.1',
       author='Marko Vidoni',
-      author_email='marko.viwa@gmail.com',
+      author_email='',
       url='https://github.com/mv1388/AIToolbox',
       description='Toolbox of useful functions often needed in different AI oriented projects',
       long_description=open('README.md').read(),
