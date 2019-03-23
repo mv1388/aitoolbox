ecc7e0a04d83a481b33515cbe81a64920cbc8917
Marko Vidoni <marko.vidoni@gmail.com>
2019-03-23 17:58:49 +0100

    Placeholder & TODO for kerastrain TrainLoop. Idea: use Keras but still make use of result package and project folder structure tracking implemented currently only for PyTorch

---
+++ AIToolbox/kerastrain/train_loop.py
@@ -0,0 +1,11 @@
+import time
+import datetime
+import numpy as np
+
+
+class TrainLoop:
+    def __init__(self, model,
+                 train_loader, validation_loader,
+                 batch_model_feed_def,
+                 optimizer, criterion):
+        pass
