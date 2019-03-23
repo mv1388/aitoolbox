5994fd868c6a8b0d56da293adfe9c90af04fdd90
Marko Vidoni <marko.vidoni@gmail.com>
2019-03-23 17:59:49 +0100

    Comment

--- AIToolbox/kerastrain/train_loop.py
+++ AIToolbox/kerastrain/train_loop.py
@@ -3,6 +3,14 @@ import datetime
 import numpy as np
 
 
+"""
+
+    Placeholder & TODO for kerastrain TrainLoop. Idea: use Keras but still make use of result package 
+    and project folder structure tracking implemented currently only for PyTorch
+
+"""
+
+
 class TrainLoop:
     def __init__(self, model,
                  train_loader, validation_loader,
