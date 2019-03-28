2e02be4bd850642e7f6ccc78b6ae3c9469246f83
Marko Vidoni <marko.vidoni@gmail.com>
2019-03-28 19:32:52 +0100

    keras example final version

--- examples/TrainLoop_use/kerastrainloop_use_example.py
+++ examples/TrainLoop_use/kerastrainloop_use_example.py
@@ -53,17 +53,17 @@ model.add(Activation('softmax'))
 optimizer = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
 
 
-TrainLoop(model, optimizer, 'categorical_crossentropy', ['accuracy'])(num_epoch=10, batch_size=batch_size,
-                                                                      x=x_train,
-                                                                      y=y_train,
-                                                                      validation_data=(x_test, y_test),
-                                                                      shuffle=True)
+# TrainLoop(model, optimizer, 'categorical_crossentropy', ['accuracy'])(num_epoch=10, batch_size=batch_size,
+#                                                                       x=x_train,
+#                                                                       y=y_train,
+#                                                                       validation_data=(x_test, y_test),
+#                                                                       shuffle=True)
 
-# TrainLoopModelCheckpointEndSave(model, optimizer, 'categorical_crossentropy', ['accuracy'],
-#                                 'kerasloop_3', 'checkpoint_endsave', '/Users/markovidoni/PycharmProjects/MemoryNet/model_results',
-#                                 {}, ClassificationResultPackage(),
-#                                 test_loader=(x_test, y_test))(num_epoch=1, batch_size=batch_size,
-#                                                               x=x_train,
-#                                                               y=y_train,
-#                                                               validation_data=(x_test, y_test),
-#                                                               shuffle=True)
+TrainLoopModelCheckpointEndSave(model, optimizer, 'categorical_crossentropy', ['accuracy'],
+                                'kerasloop_4', 'checkpoint_endsave', 'PATH',
+                                {}, ClassificationResultPackage(),
+                                test_loader=(x_test, y_test))(num_epoch=1, batch_size=batch_size,
+                                                              x=x_train,
+                                                              y=y_train,
+                                                              validation_data=(x_test, y_test),
+                                                              shuffle=True)
