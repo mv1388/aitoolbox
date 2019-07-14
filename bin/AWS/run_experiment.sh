c303712ea5835addeef5c92187f986ab9008605d
Marko Vidoni <marko.vidoni@gmail.com>
2019-07-14 19:24:56 +0200

    Terminate instance after training end

--- bin/AWS/run_experiment.sh
+++ bin/AWS/run_experiment.sh
@@ -4,14 +4,14 @@ project_root_path=~/project
 export PYTHONPATH=${PYTHONPATH}:$project_root_path
 
 terminate_cmd=$1
-aws_instance_id=$2
 
-# TODO: add tmux
 
 source $project_root_path/AWS_run_scripts/AWS_core_scripts/aws_run_experiments_project.sh $project_root_path
 
 
 if [ "$terminate_cmd" == "--terminate" ]; then
     echo Terminating the instance
+    aws_instance_id=$(ec2-metadata --instance-id | cut -d " " -f 2)
+
     aws ec2 terminate-instances --instance-ids $aws_instance_id
 fi
