de2bdc716b5e11d03755d3ec07405ee055354179
Marko Vidoni <marko.vidoni@gmail.com>
2019-07-26 21:43:43 +0200

    HyperParameter reporter

--- AIToolbox/experiment/result_reporting/hyperparam_reporter.py
+++ AIToolbox/experiment/result_reporting/hyperparam_reporter.py
@@ -1,4 +1,67 @@
+import os
+
+from AIToolbox.experiment.local_save.local_results_save import BaseLocalResultsSaver as ResultsSaver
+from AIToolbox.cloud.AWS.model_save import BaseModelSaver
+from AIToolbox.cloud.AWS.results_save import BaseResultsSaver
+from AIToolbox.cloud.GoogleCloud.model_save import BaseModelGoogleStorageSaver
+from AIToolbox.cloud.GoogleCloud.results_save import BaseResultsGoogleStorageSaver
+
 
 class HyperParameterReporter:
-    def __init__(self, experiment_folder_path):
+    def __init__(self, project_name, experiment_name, experiment_timestamp, local_model_result_folder_path):
+        """
 
+        Args:
+            project_name (str): root name of the project
+            experiment_name (str): name of the particular experiment
+            experiment_timestamp (str): time stamp of the training start
+            local_model_result_folder_path (str): root local path where project folder will be created
+        """
+        self.project_name = project_name
+        self.experiment_name = experiment_name
+        self.experiment_timestamp = experiment_timestamp
+        self.local_model_result_folder_path = os.path.expanduser(local_model_result_folder_path)
+
+        _, self.experiment_dir_path, _ = ResultsSaver.form_experiment_local_folders_paths(project_name, experiment_name,
+                                                                                          experiment_timestamp,
+                                                                                          local_model_result_folder_path)
+        ResultsSaver.create_experiment_local_folders(project_name, experiment_name, experiment_timestamp,
+                                                     local_model_result_folder_path)
+
+        self.local_args_file_path = os.path.join(self.experiment_dir_path, 'args_list.txt')
+
+    def save_args_to_text_file(self, args, sort_names=False):
+        """
+
+        Args:
+            args (dict): hyper-parameters listed in the dict
+            sort_names (bool): should presented hyper-param names be listed alphabetically
+
+        Returns:
+            str: path to the saved hyper-param text file
+        """
+        param_names = sorted(args.keys()) if sort_names else args.keys()
+
+        with open(self.local_args_file_path, 'w') as f:
+            for k in param_names:
+                f.write(f'{k}: {args[k]}')
+
+        return self.local_args_file_path
+
+    def copy_to_cloud_storage(self, local_args_file_path, cloud_saver):
+        """
+
+        Args:
+            local_args_file_path (str):
+            cloud_saver (BaseModelSaver or BaseResultsSaver or BaseModelGoogleStorageSaver or BaseResultsGoogleStorageSaver):
+
+        Returns:
+            str: path where the file was saved in the cloud storage
+        """
+        cloud_folder_path = cloud_saver \
+            .create_experiment_cloud_storage_folder_structure(self.project_name, self.experiment_name,
+                                                              self.experiment_timestamp).rsplit('/', 1)[0]
+
+        cloud_saver.save_file(local_args_file_path, cloud_folder_path)
+
+        return cloud_folder_path
