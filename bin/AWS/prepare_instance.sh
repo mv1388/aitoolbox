be372b96fbcaf27481dbc61825d12911053bb285
Marko Vidoni <marko.vidoni@gmail.com>
2019-08-03 16:34:35 +0200

    Argument parsing in the bash script

--- bin/AWS/prepare_instance.sh
+++ bin/AWS/prepare_instance.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 
 # Example how to run:
-
-# ./prepare_instance.sh <SSH_KEY_LOCATION> <INSTANCE_IP_ADDRESS> pytorch 0.2 SQuAD2 orig ~/PycharmProjects/MemoryNet
-
-
-# ./prepare_instance.sh <SSH_KEY_LOCATION> <INSTANCE_IP_ADDRESS> TF 0.2 SQuAD2 orig ~/PycharmProjects/MemoryNet
-# ./prepare_instance.sh <SSH_KEY_LOCATION> <INSTANCE_IP_ADDRESS> pytorch 0.2 cnn-dailymail abisee ~/PycharmProjects/MemoryNet
+# ./prepare_instance.sh -k <SSH_KEY_LOCATION> -e <INSTANCE_IP_ADDRESS> -f pytorch -v 0.2 -p ~/PycharmProjects/Transformer -d SQuAD2 -r orig
 
 # When you get ssh-ed to the instance finish the instance prep process by running:
-
 # ./finish_prepare_instance.sh
-
-# ./run_experiment.sh (optional: --terminate <AWS instance ID>)
-
+# ./run_experiment.sh (optional: --terminate)
 
 #######################
 
@@ -26,29 +18,95 @@
 
 #######################
 
-key_path=$1
-ec2_instance_address=$2
+# usage function
+function usage()
+{
+   cat << HEREDOC
 
-DL_framework=$3
+   Usage: $prepare_instance [--num NUM] [--time TIME_STR] [--verbose] [--dry-run]
 
-AIToolbox_version=$4
+   optional arguments:
+     -k, --key_path STR     path to ssh key
+     -e, --ec2 STR          ec2 instance Public DNS address
+     -f, --framework STR    desired deep learning framework
+     -v, --version FLOAT    AIToolbox version to be installed on ec2
+     -p, --project STR      path to the project to be optionally uploaded to the running ec2 instance
+     -d, --dataset STR      dataset to be optionally downloaded from the S3 storage directly to ec2 instance
+     -r, --preproc STR      the
+     -h, --help             show this help message and exit
 
-dataset_name=$5
-preproc_dataset=$6
+HEREDOC
+}
 
-local_project_path=$7
+key_path=
+ec2_instance_address=
+DL_framework=
+AIToolbox_version=
+local_project_path=
+dataset_name=
+preproc_dataset=
+
+while [[ $# -gt 0 ]]; do
+key="$1"
+
+case $key in
+    -k|--key_path)
+    key_path="$2"
+    shift 2 # past argument value
+    ;;
+    -e|--ec2)
+    ec2_instance_address="$2"
+    shift 2 # past argument value
+    ;;
+    -f|--framework)
+    DL_framework="$2"
+    shift 2 # past argument value
+    ;;
+    -v|--version)
+    AIToolbox_version="$2"
+    shift 2 # past argument value
+    ;;
+    -p|--project)
+    local_project_path="$2"
+    shift 2 # past argument value
+    ;;
+    -d|--dataset)
+    dataset_name="$2"
+    shift 2 # past argument value
+    ;;
+    -r|--preproc)
+    preproc_dataset="$2"
+    shift 2 # past argument value
+    ;;
+    -h|--help )
+    usage;
+    exit;
+    ;;
+    *)    # unknown option
+    echo "Don't know the argument"
+    usage;
+    exit;
+    ;;
+esac
+done
+
+if [ "$key_path" == "" ] || [ "$ec2_instance_address" == "" ] || [ "$DL_framework" == "" ] || [ "$AIToolbox_version" == "" ]; then
+    echo "Not provided required parameters"
+    usage
+    exit
+fi
 
 
-if [ $DL_framework == "TF" ]; then
+if [ "$DL_framework" == "TF" ]; then
     py_env="tensorflow_p36"
-elif [ $DL_framework == "pytorch" ]; then
+elif [ "$DL_framework" == "pytorch" ]; then
     py_env="pytorch_p36"
 else
     py_env="tensorflow_p36"
 fi
 
 
-ssh -i $key_path ec2-user@$ec2_instance_address 'mkdir ~/project ; mkdir ~/project/data ; mkdir ~/project/model_results'
+ssh -i "$key_path" ec2-user@"$ec2_instance_address" 'mkdir ~/project ; mkdir ~/project/data ; mkdir ~/project/model_results'
 
 scp -i $key_path ../../dist/AIToolbox-$AIToolbox_version.tar.gz  ec2-user@$ec2_instance_address:~/project
 scp -i $key_path download_data.sh  ec2-user@$ec2_instance_address:~/project
@@ -107,7 +165,7 @@ chmod u+x finish_prepare_instance.sh
 scp -i $key_path finish_prepare_instance.sh  ec2-user@$ec2_instance_address:~/.
 rm finish_prepare_instance.sh
 
-if [ ! -z "$local_project_path" ]; then
+if [ "$local_project_path" != '' ]; then
     echo Uploading project folder $local_project_path
     source $local_project_path/AWS_run_scripts/AWS_core_scripts/aws_project_upload.sh $key_path $ec2_instance_address "~/project" $local_project_path
 fi
