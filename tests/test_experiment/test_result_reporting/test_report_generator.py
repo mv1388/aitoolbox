46cc81f0daab233426034f68910b4da0131da043
Marko Vidoni <marko.vidoni@gmail.com>
2019-09-09 22:02:55 +0200

    Implemented test_multi_dir_write unittest

--- tests/test_experiment/test_result_reporting/test_report_generator.py
+++ tests/test_experiment/test_result_reporting/test_report_generator.py
@@ -49,3 +49,46 @@ class TestTrainingHistoryWriter(unittest.TestCase):
         project_path = os.path.join(THIS_DIR, 'results')
         if os.path.exists(project_path):
             shutil.rmtree(project_path)
+
+    def test_multi_dir_write(self):
+        train_history = TrainingHistory().wrap_pre_prepared_history({'loss': [123.4, 1223.4, 13323.4, 13323.4, 99999],
+                                                                     'accumulated_loss': [], 'val_loss': [],
+                                                                     'NEW_METRIC': [13323.4, 133323.4]})
+
+        result_writer = TrainingHistoryWriter(experiment_results_local_path=THIS_DIR)
+
+        results_file_path_in_cloud_results_dir, results_file_local_path = \
+            result_writer.generate_report(train_history, epoch=0,
+                                          file_name=f'results.txt', results_folder_name='results')
+
+        self.assertEqual(results_file_path_in_cloud_results_dir, 'results/results.txt')
+        self.assertEqual(results_file_local_path, os.path.join(THIS_DIR, 'results/results.txt'))
+
+        results_file_path_in_cloud_results_dir_2, results_file_local_path_2 = \
+            result_writer.generate_report(train_history, epoch=0,
+                                          file_name=f'results.txt', results_folder_name='results_NEW')
+
+        self.assertEqual(results_file_path_in_cloud_results_dir_2, 'results_NEW/results.txt')
+        self.assertEqual(results_file_local_path_2, os.path.join(THIS_DIR, 'results_NEW/results.txt'))
+
+        with open(os.path.join(THIS_DIR, 'results/results.txt'), 'r') as f:
+            f_content = [l.strip() for l in f.readlines()]
+
+        self.assertEqual(f_content,
+                         ['============================', 'Epoch: 0', '============================',
+                          'loss:\t99999', 'NEW_METRIC:\t133323.4', '', ''])
+
+        with open(os.path.join(THIS_DIR, 'results_NEW/results.txt'), 'r') as f:
+            f_content = [l.strip() for l in f.readlines()]
+
+        self.assertEqual(f_content,
+                         ['============================', 'Epoch: 0', '============================',
+                          'loss:\t99999', 'NEW_METRIC:\t133323.4', '', ''])
+
+        project_path = os.path.join(THIS_DIR, 'results')
+        if os.path.exists(project_path):
+            shutil.rmtree(project_path)
+
+        project_path = os.path.join(THIS_DIR, 'results_NEW')
+        if os.path.exists(project_path):
+            shutil.rmtree(project_path)
