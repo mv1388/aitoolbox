e9f7e53f03bd06b9be89088cb37f0c4d0d2c1980
Marko Vidoni <marko.vidoni@gmail.com>
2019-09-15 09:32:13 +0200

    Style fix 2

--- AIToolbox/torchtrain/data/batch_model_feed_defs.py
+++ AIToolbox/torchtrain/data/batch_model_feed_defs.py
@@ -2,11 +2,9 @@ from abc import ABC, abstractmethod
 import torch
 
 
-'''
-    Class / Functions defining the handling of a single batch and feeding it into the PyTorch model
-
-    Such a function is supplied as an argument to the main train loop code
-'''
+# Class / Functions defining the handling of a single batch and feeding it into the PyTorch model
+#
+# Such a function is supplied as an argument to the main train loop code
 
 
 class AbstractModelFeedDefinition(ABC):
