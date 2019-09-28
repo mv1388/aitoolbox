09311a97842229da8422b646b075781209174e9f
Marko Vidoni <marko.vidoni@gmail.com>
2019-09-28 16:12:18 +0200

    Refactor model loading and implement s3 model loader

--- AIToolbox/cloud/AWS/model_load.py
+++ AIToolbox/cloud/AWS/model_load.py
@@ -2,82 +2,98 @@ import os
 
 from AIToolbox.cloud.AWS.data_access import BaseDataLoader
 from AIToolbox.experiment.local_load.local_model_load import AbstractLocalModelLoader, PyTorchLocalModelLoader
+from AIToolbox.experiment.local_save.folder_create import ExperimentFolderCreator
 
 
 class BaseModelLoader(BaseDataLoader):
-    def __init__(self, local_model_loader, project_name, experiment_folder, model_save_dir,
-                 local_model_result_folder_path, pretrained_model_dir='pretrained_models',
-                 bucket_name='model-result'):
+    def __init__(self, local_model_loader, local_model_result_folder_path='~/project/model_result',
+                 bucket_name='model-result', cloud_dir_prefix=''):
         """Base saved model loading from S3 storage
 
         Args:
             local_model_loader (AbstractLocalModelLoader): model loader implementing the loading of the saved model for
                 the selected deep learning framework
-            project_name (str): root name of the project
-            experiment_folder (str): name of the particular experiment. Not to be mistaken for model saver's
-                experiment_name parameter. As the framework currently operates, it appends the timestamp at the end of
-                the experiment name. Consequently, the experiment_folder = experiment_name + _timestamp.
-            model_save_dir (str): name of the folder inside experiment folder where the model is saved
             local_model_result_folder_path (str): root local path where project folder will be created
-            pretrained_model_dir (str): if dealing with pre-trained models download them locally into a separate folder
-                in order not to mix the downloaded pre-trained models with those which will be subsequently trained
             bucket_name (str): name of the bucket in the cloud storage from which the model will be downloaded
+            cloud_dir_prefix (str): path to the folder inside the bucket where the experiments are going to be saved
         """
         BaseDataLoader.__init__(self, bucket_name, local_model_result_folder_path)
+        self.local_model_result_folder_path = self.local_base_data_folder_path
 
-        self.project_name = project_name
-        self.experiment_folder = experiment_folder
-
-        self.pretrained_model_dir = pretrained_model_dir
-        self.cloud_model_folder_path = os.path.join(project_name, experiment_folder, model_save_dir)
-        self.local_model_folder_path = os.path.join(self.local_base_data_folder_path, self.pretrained_model_dir)
-
+        self.cloud_dir_prefix = cloud_dir_prefix
         self.local_model_loader = local_model_loader
 
         if not isinstance(local_model_loader, AbstractLocalModelLoader):
             raise TypeError('Provided local_model_loader is not inherited from AbstractLocalModelLoader as required.')
 
-    def load_model(self, model_name, **kwargs):
+    def load_model(self, project_name, experiment_name, experiment_timestamp,
+                   model_save_dir='checkpoint_model', epoch_num=None,
+                   **kwargs):
         """Download and read/load the model
 
         Args:
-            model_name (str): model file name
-            **kwargs:
+            project_name (str): root name of the project
+            experiment_name (str): name of the particular experiment
+            experiment_timestamp (str): time stamp at the start of training
+            model_save_dir (str): name of the folder inside experiment folder where the model is saved
+            epoch_num (int or None): epoch number of the model checkpoint or none if loading final model
+            **kwargs: additional local_model_loader parameters
 
         Returns:
             dict: model representation. (currently only returning dicts as only PyTorch model loading is supported)
         """
-        self.exists_local_data_folder(self.pretrained_model_dir)
+        cloud_model_folder_path = os.path.join(self.cloud_dir_prefix,
+                                               project_name,
+                                               experiment_name + '_' + experiment_timestamp,
+                                               model_save_dir)
+        experiment_dir_path = ExperimentFolderCreator.create_experiment_base_folder(project_name, experiment_name,
+                                                                                    experiment_timestamp,
+                                                                                    self.local_model_result_folder_path)
+        local_model_folder_path = os.path.join(experiment_dir_path, model_save_dir)
+        if not os.path.exists(local_model_folder_path):
+            os.mkdir(local_model_folder_path)
+
+        if epoch_num is None:
+            model_name = f'model_{experiment_name}_{experiment_timestamp}.pth'
+        else:
+            model_name = f'model_{experiment_name}_{experiment_timestamp}_E{epoch_num}.pth'
 
         # Loads the model save file from S3 to the local folder
-        cloud_model_file_path = os.path.join(self.cloud_model_folder_path, model_name)
-        local_model_file_path = os.path.join(self.local_model_folder_path, model_name)
+        cloud_model_file_path = os.path.join(cloud_model_folder_path, model_name)
+        local_model_file_path = os.path.join(local_model_folder_path, model_name)
 
+        # Will only download from S3 if file not present on local drive
         self.load_file(cloud_model_file_path, local_model_file_path)
 
-        return self.local_model_loader.load_model(model_name, self.project_name, self.experiment_folder, **kwargs)
+        return self.local_model_loader.load_model(project_name, experiment_name, experiment_timestamp,
+                                                  model_save_dir, epoch_num, **kwargs)
 
 
 class PyTorchS3ModelLoader(BaseModelLoader):
-    def __init__(self, project_name, experiment_folder, model_save_dir,
-                 local_model_result_folder_path, pretrained_model_dir='pretrained_models',
-                 bucket_name='model-result'):
+    def __init__(self, local_model_result_folder_path='~/project/model_result',
+                 bucket_name='model-result', cloud_dir_prefix=''):
         """PyTorch S3 model downloader & loader
 
         Args:
-            project_name (str): root name of the project
-            experiment_folder (str): name of the particular experiment Not to be mistaken for model saver's
-                experiment_name parameter. As the framework currently operates, it appends the timestamp at the end of
-                the experiment name. Consequently, the experiment_folder = experiment_name + _timestamp.
-            model_save_dir (str): name of the folder inside experiment folder where the model is saved
             local_model_result_folder_path (str): root local path where project folder will be created
-            pretrained_model_dir (str): if dealing with pre-trained models download them locally into a separate folder
-                in order not to mix the downloaded pre-trained models with those which will be subsequently trained
             bucket_name (str): name of the bucket in the cloud storage from which the model will be downloaded
+            cloud_dir_prefix (str): path to the folder inside the bucket where the experiments are going to be saved
         """
-        local_model_folder_path = os.path.expanduser(os.path.join(local_model_result_folder_path, pretrained_model_dir))
-        local_model_loader = PyTorchLocalModelLoader(local_model_folder_path)
+        local_model_loader = PyTorchLocalModelLoader(local_model_result_folder_path)
 
-        BaseModelLoader.__init__(self, local_model_loader,
-                                 project_name, experiment_folder, model_save_dir,
-                                 local_model_result_folder_path, pretrained_model_dir, bucket_name)
+        BaseModelLoader.__init__(self, local_model_loader, local_model_result_folder_path,
+                                 bucket_name, cloud_dir_prefix)
+
+    def init_model(self, model, used_data_parallel=False):
+        """Initialize provided PyTorch model with the loaded model weights
+
+        For this function to work, load_model() must be first called to read the model representation into memory.
+
+        Args:
+            model: PyTorch model
+            used_data_parallel: if the saved model was nn.DataParallel or normal model
+
+        Returns:
+
+        """
+        return self.local_model_loader.init_model(model, used_data_parallel)
--- AIToolbox/experiment/local_load/local_model_load.py
+++ AIToolbox/experiment/local_load/local_model_load.py
@@ -1,20 +1,23 @@
 from abc import ABC, abstractmethod
 import os
 from collections import OrderedDict
-
 import torch
+
+from AIToolbox.experiment.local_save.folder_create import ExperimentFolderCreator
 
 
 class AbstractLocalModelLoader(ABC):
     @abstractmethod
-    def load_model(self, model_name, project_name, experiment_name, **kwargs):
+    def load_model(self, project_name, experiment_name, experiment_timestamp, model_save_dir, epoch_num=None, **kwargs):
         """Model loading method all the model loaders need to implement
 
         Args:
-            model_name (str): model file name
             project_name (str): root name of the project
             experiment_name (str): name of the particular experiment
-            **kwargs:
+            experiment_timestamp (str): time stamp at the start of training
+            model_save_dir (str): name of the folder inside experiment folder where the model is saved
+            epoch_num (int or None): epoch number of the model checkpoint or none if loading final model
+            **kwargs: additional parameters for specific framework model loader
 
         Returns:
 
@@ -30,33 +33,57 @@ class PyTorchLocalModelLoader(AbstractLocalModelLoader):
             local_model_result_folder_path (str): root local path where project folder will be created
         """
         self.local_model_result_folder_path = os.path.expanduser(local_model_result_folder_path)
-        self.model_load = None
+        self.model_representation = None
 
-    def load_model(self, model_name, project_name, experiment_name, map_location=None):
-        """
+    def load_model(self, project_name, experiment_name, experiment_timestamp, model_save_dir='checkpoint_model',
+                   epoch_num=None, map_location=None):
+        """Model loading interface compatible with the experiment folder structure maintained by the AIToolbox TrainLoop
 
         Args:
-            model_name (str): model file name
             project_name (str): root name of the project
             experiment_name (str): name of the particular experiment
+            experiment_timestamp (str): time stamp at the start of training
+            model_save_dir (str): name of the folder inside experiment folder where the model is saved
+            epoch_num (int or None): epoch number of the model checkpoint or none if loading final model
             map_location (str or None):
 
         Returns:
-
+            model
         """
-        # model_path = os.path.join(self.local_model_result_folder_path, project_name, experiment_name, model_name)
-        model_path = os.path.join(self.local_model_result_folder_path, model_name)
+        _, experiment_dir_path = ExperimentFolderCreator.get_experiment_base_folder_paths(project_name, experiment_name,
+                                                                                          experiment_timestamp,
+                                                                                          self.local_model_result_folder_path)
+        if epoch_num is None:
+            model_name = f'model_{experiment_name}_{experiment_timestamp}.pth'
+        else:
+            model_name = f'model_{experiment_name}_{experiment_timestamp}_E{epoch_num}.pth'
 
-        self.model_load = torch.load(model_path, map_location=map_location)
+        model_path = os.path.join(experiment_dir_path, model_save_dir, model_name)
 
-        return self.model_load
+        self.model_representation = torch.load(model_path, map_location=map_location)
+        return self.model_representation
+
+    def load_model_from_path(self, model_path, map_location=None):
+        """General model loading when the AIToolbox TrainLoop experiment folder structure is not used
+
+        Args:
+            model_path (str):
+            map_location (str or None):
+
+        Returns:
+            model
+        """
+        self.model_representation = torch.load(model_path, map_location=map_location)
+        return self.model_representation
 
     def check_if_model_loaded(self):
-        if self.model_load is None:
+        if self.model_representation is None:
             raise ValueError('Model has not yet been loaded. Please call load_model() first.')
 
     def init_model(self, model, used_data_parallel=False):
-        """
+        """Initialize provided PyTorch model with the loaded model weights
+
+        For this function to work, load_model() must be first called to read the model representation into memory.
 
         Args:
             model: PyTorch model
@@ -67,11 +94,11 @@ class PyTorchLocalModelLoader(AbstractLocalModelLoader):
         """
         self.check_if_model_loaded()
 
-        state_dict = self.model_load['model_state_dict']
+        state_dict = self.model_representation['model_state_dict']
 
         if used_data_parallel:
             state_dict = OrderedDict()
-            for k, v in self.model_load['model_state_dict'].items():
+            for k, v in self.model_representation['model_state_dict'].items():
                 name = k[7:]  # remove `module.`
                 state_dict[name] = v
 
@@ -90,7 +117,7 @@ class PyTorchLocalModelLoader(AbstractLocalModelLoader):
         """
         self.check_if_model_loaded()
 
-        optimizer.load_state_dict(self.model_load['optimizer_state_dict'])
+        optimizer.load_state_dict(self.model_representation['optimizer_state_dict'])
 
         # for state in optimizer.state.values():
         #     for k, v in state.items():
