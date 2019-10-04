66ab71e7d36204dbb887790ec72179508e203142
Marko Vidoni <marko.vidoni@gmail.com>
2019-10-04 15:36:43 +0200

    Enable multi-optimizer matching in MultiLoss

--- AIToolbox/torchtrain/multi_loss.py
+++ AIToolbox/torchtrain/multi_loss.py
@@ -8,13 +8,21 @@ except AttributeError:
 
 
 class MultiLoss:
-    def __init__(self, loss_list):
+    def __init__(self, loss_list, amp_optimizer_order=None):
         """Multiple loss wrapper for TrainLoop based training
 
         Args:
             loss_list (list): list of loss objects which are used to calculate losses in the TrainLoop
+            amp_optimizer_order (lambda or None): when using Nvidia Apex AMP, construct a list of optimizers
+                where the optimizer's position matches the position of the corresponding loss in the loss_list.
+                This is useful in cases similar to this:
+                https://github.com/NVIDIA/apex/tree/master/examples/dcgan#mixed-precision-dcgan-training-in-pytorch
+
+                When not using Nvidia Apex AMP this can be ignored as losses and optimizers don't need to be
+                explicitly paired.
         """
         self.loss_list = loss_list
+        self.amp_optimizer_order = amp_optimizer_order
 
     def backward(self):
         for loss in self.loss_list:
@@ -24,12 +32,14 @@ class MultiLoss:
         """Executes backward() over all the losses using the list o optimizers
 
         Args:
-            optimizers (list): list of optimizers. Positions in the list correspond to the ordering of the losses
+            optimizers (list): list of optimizers all used optimizers
 
         Returns:
             None
         """
-        for i, (loss, optimizer) in enumerate(zip(self.loss_list, optimizers)):
+        optimizers_list = optimizers if self.amp_optimizer_order is None else self.amp_optimizer_order(optimizers)
+
+        for i, (loss, optimizer) in enumerate(zip(self.loss_list, optimizers_list)):
             with amp.scale_loss(loss, optimizer, loss_id=i) as scaled_loss:
                 scaled_loss.backward()
 
