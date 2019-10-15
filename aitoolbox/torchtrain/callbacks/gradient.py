dd68a6f558095b65a4f2bfebac8347cfa171c19c
Marko Vidoni <marko.vidoni@gmail.com>
2019-10-15 06:59:17 +0200

    Use gradient base in the grad norm clip cb

--- aitoolbox/torchtrain/callbacks/gradient.py
+++ aitoolbox/torchtrain/callbacks/gradient.py
@@ -23,7 +23,7 @@ class GradientCallbackBase(AbstractCallback):
         self.train_loop_obj.grad_cb_used = True
 
 
-class GradNormClip(AbstractCallback):
+class GradNormClip(GradientCallbackBase):
     def __init__(self, max_norm, **kwargs):
         """Gradient norm clipping
 
@@ -31,12 +31,9 @@ class GradNormClip(AbstractCallback):
             max_norm (int or float): gradient clipping
             **kwargs:
         """
-        AbstractCallback.__init__(self, 'Gradient clipping')
+        GradientCallbackBase.__init__(self, 'Gradient clipping')
         self.max_norm = max_norm
         self.kwargs = kwargs
-
-    def on_train_loop_registration(self):
-        self.train_loop_obj.grad_cb_used = True
 
     def on_after_gradient_update(self):
         torch.nn.utils.clip_grad_norm_(self.train_loop_obj.model.parameters(), self.max_norm, **self.kwargs)
