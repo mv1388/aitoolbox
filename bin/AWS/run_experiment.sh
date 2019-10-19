09d855f3c4ba4d554eea990ca33114440ea0d1ce
Marko Vidoni <marko.vidoni@gmail.com>
2019-10-19 15:14:15 +0200

    Add -t option to terminate after running the experiment

--- bin/AWS/run_experiment.sh
+++ bin/AWS/run_experiment.sh
@@ -2,7 +2,7 @@
 
 # When you get ssh-ed to the instance finish the instance prep process by running:
 # ./finish_prepare_instance.sh
-# ./run_experiment.sh (optional: --terminate)
+# ./run_experiment.sh (optional: -t / --terminate)
 
 project_root_path=~/project
 export PYTHONPATH=${PYTHONPATH}:$project_root_path
@@ -13,7 +13,7 @@ terminate_cmd=$1
 source $project_root_path/AWS_run_scripts/AWS_core_scripts/aws_run_experiments_project.sh $project_root_path
 
 
-if [ "$terminate_cmd" == "--terminate" ]; then
+if [[ "$terminate_cmd" == "-t"  || "$terminate_cmd" == "--terminate" ]]; then
     echo Terminating the instance
     aws_instance_id=$(ec2-metadata --instance-id | cut -d " " -f 2)
 
