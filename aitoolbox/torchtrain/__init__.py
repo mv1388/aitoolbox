50538f23ac810c1cd6e5135bd8fcde665737c0b7
Marko Vidoni <marko.vidoni@gmail.com>
2019-10-21 23:50:06 +0200

    Quick import for torachtrain

---
+++ aitoolbox/torchtrain/__init__.py
@@ -0,0 +1,6 @@
+from aitoolbox.torchtrain.model import TTModel, TTDataParallel, ModelWrap
+from aitoolbox.torchtrain.train_loop import TrainLoop, TrainLoopModelCheckpoint, TrainLoopModelEndSave, TrainLoopModelCheckpointEndSave
+from aitoolbox.torchtrain.data.batch_model_feed_defs import AbstractModelFeedDefinition
+
+from aitoolbox.torchtrain.data.dataset import BasicDataset
+
