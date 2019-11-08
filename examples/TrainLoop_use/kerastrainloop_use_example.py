0af7687e7e360a6d2cdf728696405a7d74224ace
Marko Vidoni <marko.vidoni@gmail.com>
2019-11-08 21:27:17 +0100

    Fix code style problems

--- examples/TrainLoop_use/kerastrainloop_use_example.py
+++ examples/TrainLoop_use/kerastrainloop_use_example.py
@@ -1,11 +1,9 @@
 from __future__ import print_function
 import keras
 from keras.datasets import cifar10
-from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-import os
 
 from aitoolbox.kerastrain.train_loop import TrainLoop, TrainLoopModelCheckpointEndSave
 from aitoolbox.experiment.result_package.basic_packages import ClassificationResultPackage
