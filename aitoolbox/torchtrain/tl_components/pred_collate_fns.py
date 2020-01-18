d74e081749b31ec3afc500cae735c8b89cd21c47
Marko Vidoni <marko.vidoni@gmail.com>
2020-01-18 11:36:45 +0100

    Add docstring content

--- aitoolbox/torchtrain/tl_components/pred_collate_fns.py
+++ aitoolbox/torchtrain/tl_components/pred_collate_fns.py
@@ -2,11 +2,29 @@ import torch
 
 
 def append_predictions(y_batch, predictions):
+    """
+
+    Args:
+        y_batch (torch.Tensor): predictions for the new batch
+        predictions (list): accumulation list where all the batched predictions are appended
+
+    Returns:
+        list: predictions list with the new tensor appended
+    """
     predictions.append(y_batch)
     return predictions
 
 
 def append_concat_predictions(y_batch, predictions):
+    """
+
+    Args:
+        y_batch (torch.Tensor or list):
+        predictions (list): accumulation list where all the batched predictions are added
+
+    Returns:
+        list: predictions list with the new tensor appended
+    """
     if isinstance(y_batch, list):
         predictions += y_batch
     else:
@@ -16,8 +34,24 @@ def append_concat_predictions(y_batch, predictions):
 
 
 def torch_cat_transf(predictions):
+    """PyTorch concatenation of the given list of tensors
+
+    Args:
+        predictions (list): expects a list of torch.Tensor
+
+    Returns:
+        torch.Tensor: concatenated tensor made up of provided smaller tensors
+    """
     return torch.cat(predictions)
 
 
 def keep_list_transf(predictions):
+    """Identity transformation of the predictions keeping them as they were
+
+    Args:
+        predictions (list): list of predictions
+
+    Returns:
+        list: returns unaltered list of predictions
+    """
     return predictions
