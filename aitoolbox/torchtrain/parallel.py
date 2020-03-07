9ee74cdc3ff61c479838e19afeed0188b574d75c
Marko Vidoni <marko.vidoni@gmail.com>
2020-03-07 13:25:18 +0100

    Simplified parallel wrappers

--- aitoolbox/torchtrain/parallel.py
+++ aitoolbox/torchtrain/parallel.py
@@ -11,7 +11,7 @@ except ImportError:
 from aitoolbox.utils.util import copy_function
 
 
-class TTDataParallel(nn.DataParallel):
+class TTDataParallelExplicit(nn.DataParallel):
     def __init__(self, module, add_model_attributes=None,
                  default_model_methods=('get_loss', 'get_loss_eval', 'get_predictions'), **kwargs):
         """torchtrain enabled DataParallel
@@ -58,7 +58,7 @@ class TTDataParallel(nn.DataParallel):
         return self.get_predictions_fn(self, batch_data, device)
 
 
-class TTDistributedDataParallel(DistributedDataParallel):
+class TTDistributedDataParallelExplicit(DistributedDataParallel):
     def __init__(self, module, add_model_attributes=None,
                  default_model_methods=('get_loss', 'get_loss_eval', 'get_predictions'), **kwargs):
         """torchtrain enabled DistributedDataParallel
@@ -101,7 +101,7 @@ class TTDistributedDataParallel(DistributedDataParallel):
         return self.get_predictions_fn(self, batch_data, device)
 
 
-class TTApexDistributedDataParallel(ApexDistributedDataParallel):
+class TTApexDistributedDataParallelExplicit(ApexDistributedDataParallel):
     def __init__(self, module, add_model_attributes=None,
                  default_model_methods=('get_loss', 'get_loss_eval', 'get_predictions'), **kwargs):
         """torchtrain enabled Nvidia Apex DistributedDataParallel
@@ -142,3 +142,57 @@ class TTApexDistributedDataParallel(ApexDistributedDataParallel):
 
     def get_predictions(self, batch_data, device):
         return self.get_predictions_fn(self, batch_data, device)
+
+
+class TTParallelBase:
+    def __init__(self, module, add_model_attributes=None,
+                 default_model_methods=('get_loss', 'get_loss_eval', 'get_predictions')):
+        # Core TTModel methods which every model has
+        self.get_loss_fn = copy_function(module.get_loss)
+        self.get_loss_eval_fn = copy_function(module.get_loss_eval)
+        self.get_predictions_fn = copy_function(module.get_predictions)
+
+        # Transfer any additional sub-methods from TTModel to TTDataParallel
+        methods = list(set(dir(module))
+                       - set(dir(nn.Module)) - set(vars(module)['_modules'].keys()) - set(default_model_methods))
+        additional_methods = [method_name for method_name in methods if callable(getattr(module, method_name, None))]
+
+        for method_name in additional_methods:
+            setattr(self, method_name,
+                    functools.partial(copy_function(getattr(module, method_name)), self))
+
+        # Optionally transfer additional TTModel attributes to the TTDataParallel level
+        if add_model_attributes is not None and isinstance(add_model_attributes, (list, tuple)):
+            for attr_name in add_model_attributes:
+                setattr(self, attr_name, getattr(module, attr_name))
+
+    def get_loss(self, batch_data, criterion, device):
+        return self.get_loss_fn(self, batch_data, criterion, device)
+
+    def get_loss_eval(self, batch_data, criterion, device):
+        return self.get_loss_eval_fn(self, batch_data, criterion, device)
+
+    def get_predictions(self, batch_data, device):
+        return self.get_predictions_fn(self, batch_data, device)
+
+
+class TTDataParallel(nn.DataParallel, TTParallelBase):
+    def __init__(self, module, add_model_attributes=None,
+                 default_model_methods=('get_loss', 'get_loss_eval', 'get_predictions'), **kwargs):
+        nn.DataParallel.__init__(self, module, **kwargs)
+        TTParallelBase.__init__(self, module, add_model_attributes, default_model_methods)
+
+
+class TTDistributedDataParallel(DistributedDataParallel, TTParallelBase):
+    def __init__(self, module, add_model_attributes=None,
+                 default_model_methods=('get_loss', 'get_loss_eval', 'get_predictions'), **kwargs):
+        DistributedDataParallel.__init__(self, module, **kwargs)
+        TTParallelBase.__init__(self, module, add_model_attributes, default_model_methods)
+
+
+if APEX_AVAILABLE:
+    class TTApexDistributedDataParallel(ApexDistributedDataParallel, TTParallelBase):
+        def __init__(self, module, add_model_attributes=None,
+                     default_model_methods=('get_loss', 'get_loss_eval', 'get_predictions'), **kwargs):
+            ApexDistributedDataParallel.__init__(self, module, **kwargs)
+            TTParallelBase.__init__(self, module, add_model_attributes, default_model_methods)
