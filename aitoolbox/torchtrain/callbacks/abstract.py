c5ae60a327425e995f92ecfb1fd8f59a9b8eb2f6
Marko Vidoni <marko.vidoni@gmail.com>
2020-03-08 10:14:41 +0100

    Code style improvement 2

--- aitoolbox/torchtrain/callbacks/abstract.py
+++ aitoolbox/torchtrain/callbacks/abstract.py
@@ -120,7 +120,7 @@ class AbstractCallback:
 
         """
         pass
-    
+
     def on_multiprocess_start(self):
         """Logic executed after a new multiprocessing process is spawned at the beginning of every child process
 
