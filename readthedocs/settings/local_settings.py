147c0c06ebfea73fe8cd9d66071394643295f75d
Marko Vidoni <marko.vidoni@gmail.com>
2020-04-11 17:24:09 +0200

    readthedocs local settings

--- readthedocs/settings/local_settings.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Directory that the project lives in, aka ../..
-SITE_ROOT = '/'.join(os.path.dirname(__file__).split('/')[0:-2])
-
-TEMPLATE_DIRS = (
-    "%s/_templates/apidoc/" % SITE_ROOT,  # Your custom template directory, before the RTD one to override it.
-    '%s/readthedocs/templates/' % SITE_ROOT,  # Default RTD template dir
-)
