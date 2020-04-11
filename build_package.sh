8c42828c89a8ac02ab5727c0151f4ecd85ac7705
Marko Vidoni <marko.vidoni@gmail.com>
2020-04-11 19:51:56 +0200

    Build package support for document building

--- build_package.sh
+++ build_package.sh
@@ -2,6 +2,24 @@
 
 source activate py36
 
+build_documentation=false
+
+while [[ $# -gt 0 ]]; do
+key="$1"
+
+case $key in
+    -d|--docu)
+    build_documentation=true
+    shift 1 # past argument value
+    ;;
+    *)    # unknown option
+    echo "Don't know the argument"
+    usage;
+    ;;
+esac
+done
+
+
 python setup.py test
 
 python setup.py sdist
@@ -9,3 +27,8 @@ python setup.py sdist
 rm -r aitoolbox.egg-info
 rm -r ./.eggs/
 git add -A dist/
+
+
+if [ "$build_documentation" == true ]; then
+    ./doc_build.sh
+fi
