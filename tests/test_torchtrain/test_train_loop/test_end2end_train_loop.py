9ef77174e374c178a2ceb08e4c14871a73d1f944
Marko Vidoni <marko.vidoni@gmail.com>
2020-04-15 21:24:19 +0200

    Test e2e predictions

--- tests/test_torchtrain/test_train_loop/test_end2end_train_loop.py
+++ tests/test_torchtrain/test_train_loop/test_end2end_train_loop.py
@@ -45,7 +45,7 @@ class FFNet(TTModel):
 
         predicted = self(input_data)
 
-        return predicted.cpu(), target, {}
+        return predicted.cpu(), target, {'example_feat_sum': input_data.sum(dim=1).tolist()}
 
 
 class TestEnd2EndTrainLoop(unittest.TestCase):
@@ -106,3 +106,51 @@ class TestEnd2EndTrainLoop(unittest.TestCase):
         self.assertEqual(train_loop.evaluate_loss_on_train_set(), 1.8507084131240845)
         self.assertEqual(train_loop.evaluate_loss_on_validation_set(), 2.4111196994781494)
         self.assertEqual(train_loop.evaluate_loss_on_test_set(), 2.31626296043396)
+
+    def test_e2e_ff_net_train_loop_predictions(self):
+        batch_size = 10
+
+        train_dataset = TensorDataset(torch.randn(100, 50), torch.randint(low=0, high=10, size=(100,)))
+        val_dataset = TensorDataset(torch.randn(30, 50), torch.randint(low=0, high=10, size=(30,)))
+        test_dataset = TensorDataset(torch.randn(30, 50), torch.randint(low=0, high=10, size=(30,)))
+
+        train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
+        val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
+        test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
+
+        model = FFNet()
+        optimizer = optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999))
+        criterion = nn.NLLLoss()
+
+        train_loop = TrainLoop(
+            model,
+            train_dataloader, val_dataloader, test_dataloader,
+            optimizer, criterion
+        )
+        train_loop.fit(num_epochs=5)
+
+        train_pred, train_target, train_meta = train_loop.predict_on_train_set()
+        self.assertEqual(
+            train_pred.argmax(dim=1).tolist(),
+            [0, 4, 8, 0, 1, 1, 6, 1, 6, 1, 8, 3, 0, 0, 8, 8, 1, 8, 8, 1, 8, 0, 8, 0, 0, 1, 3, 4, 8, 8, 0, 8, 8, 1, 8, 8,
+             5, 5, 1, 8, 8, 8, 8, 8, 9, 0, 8, 8, 5, 8, 8, 1, 1, 8, 5, 1, 8, 8, 5, 5, 1, 8, 8, 8, 8, 0, 0, 1, 1, 0, 8, 8,
+             3, 0, 5, 0, 8, 9, 1, 8, 8, 8, 5, 8, 5, 8, 0, 1, 8, 5, 8, 6, 5, 1, 8, 1, 0, 8, 1, 8]
+        )
+        self.assertEqual(train_target.tolist(), train_dataset.tensors[1].tolist())
+        self.assertEqual(train_dataset.tensors[0].sum(dim=1).tolist(), train_meta['example_feat_sum'])
+
+        val_pred, val_target, val_meta = train_loop.predict_on_validation_set()
+        self.assertEqual(
+            val_pred.argmax(dim=1).tolist(),
+            [1, 1, 1, 1, 5, 8, 0, 8, 1, 1, 5, 8, 8, 1, 8, 8, 1, 8, 8, 8, 1, 0, 0, 8, 0, 1, 1, 0, 1, 8]
+        )
+        self.assertEqual(val_target.tolist(), val_dataset.tensors[1].tolist())
+        self.assertEqual(val_dataset.tensors[0].sum(dim=1).tolist(), val_meta['example_feat_sum'])
+
+        test_pred, test_target, test_meta = train_loop.predict_on_test_set()
+        self.assertEqual(
+            test_pred.argmax(dim=1).tolist(),
+            [4, 8, 0, 8, 1, 8, 1, 1, 8, 8, 8, 8, 8, 0, 8, 8, 5, 8, 8, 5, 8, 1, 0, 5, 1, 8, 8, 8, 8, 1]
+        )
+        self.assertEqual(test_target.tolist(), test_dataset.tensors[1].tolist())
+        self.assertEqual(test_dataset.tensors[0].sum(dim=1).tolist(), test_meta['example_feat_sum'])
