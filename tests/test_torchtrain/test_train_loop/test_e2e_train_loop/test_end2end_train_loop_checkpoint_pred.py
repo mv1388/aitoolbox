cc1802218a8f031c314c16465d63cb76d0c8f351
Marko Vidoni <marko.vidoni@gmail.com>
2020-04-17 19:38:01 +0200

    test_e2e_ff_net_local_model_load_callback_predict

--- tests/test_torchtrain/test_train_loop/test_e2e_train_loop/test_end2end_train_loop_checkpoint_pred.py
+++ tests/test_torchtrain/test_train_loop/test_e2e_train_loop/test_end2end_train_loop_checkpoint_pred.py
@@ -16,6 +16,7 @@ from aitoolbox.torchtrain.model import TTModel
 from aitoolbox.experiment.result_package.basic_packages import ClassificationResultPackage
 from aitoolbox.torchtrain.model_predict import PyTorchModelPredictor
 from aitoolbox.experiment.local_load.local_model_load import PyTorchLocalModelLoader
+from aitoolbox.torchtrain.callbacks.model_load import ModelLoadContinueTraining
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -244,6 +245,77 @@ class TestEnd2EndTrainLoopModelSaveReloadPrediction(unittest.TestCase):
             train_dataloader, val_dataloader, test_dataloader,
             None, criterion
         )
+
+        train_pred_reload, _, _ = train_loop_reload.predict_on_train_set()
+        val_pred_reload, _, _ = train_loop_reload.predict_on_validation_set()
+        test_pred_reload, _, _ = train_loop_reload.predict_on_test_set()
+
+        train_loss_reload = train_loop_reload.evaluate_loss_on_train_set()
+        val_loss_reload = train_loop_reload.evaluate_loss_on_validation_set()
+        test_loss_reload = train_loop_reload.evaluate_loss_on_test_set()
+
+        self.assertEqual(train_pred.tolist(), train_pred_reload.tolist())
+        self.assertEqual(val_pred.tolist(), val_pred_reload.tolist())
+        self.assertEqual(test_pred.tolist(), test_pred_reload.tolist())
+
+        self.assertEqual(train_loss, train_loss_reload)
+        self.assertEqual(val_loss, val_loss_reload)
+        self.assertEqual(test_loss, test_loss_reload)
+
+        project_path = os.path.join(THIS_DIR, 'e2e_train_loop_example')
+        if os.path.exists(project_path):
+            shutil.rmtree(project_path)
+
+    def test_e2e_ff_net_local_model_load_callback_predict(self):
+        self.set_seeds()
+        batch_size = 10
+
+        train_dataset = TensorDataset(torch.randn(100, 50), torch.randint(low=0, high=10, size=(100,)))
+        val_dataset = TensorDataset(torch.randn(30, 50), torch.randint(low=0, high=10, size=(30,)))
+        test_dataset = TensorDataset(torch.randn(30, 50), torch.randint(low=0, high=10, size=(30,)))
+
+        train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
+        val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
+        test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
+
+        model = FFNet()
+        optimizer = optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999))
+        criterion = nn.NLLLoss()
+
+        train_loop = TrainLoopCheckpointEndSave(
+            model,
+            train_dataloader, val_dataloader, test_dataloader,
+            optimizer, criterion,
+            project_name='e2e_train_loop_example', experiment_name='TrainLoopCheckpointEndSave_example',
+            local_model_result_folder_path=THIS_DIR,
+            hyperparams={'batch_size': batch_size},
+            val_result_package=ClassificationResultPackage(), test_result_package=ClassificationResultPackage(),
+            cloud_save_mode=None
+        )
+        train_loop.fit(num_epochs=5)
+
+        train_pred, _, _ = train_loop.predict_on_train_set()
+        val_pred, _, _ = train_loop.predict_on_validation_set()
+        test_pred, _, _ = train_loop.predict_on_test_set()
+
+        train_loss = train_loop.evaluate_loss_on_train_set()
+        val_loss = train_loop.evaluate_loss_on_validation_set()
+        test_loss = train_loop.evaluate_loss_on_test_set()
+
+        model_reloaded = FFNet()
+        optimizer_reloaded = optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999))
+
+        train_loop_reload = TrainLoop(
+            model_reloaded,
+            train_dataloader, val_dataloader, test_dataloader,
+            optimizer_reloaded, criterion
+        )
+        train_loop_reload.callbacks_handler.register_callbacks([
+            ModelLoadContinueTraining(train_loop.experiment_timestamp, saved_model_dir='model',
+                                      project_name=train_loop.project_name, experiment_name=train_loop.experiment_name,
+                                      local_model_result_folder_path=THIS_DIR,
+                                      cloud_save_mode='local')
+        ])
 
         train_pred_reload, _, _ = train_loop_reload.predict_on_train_set()
         val_pred_reload, _, _ = train_loop_reload.predict_on_validation_set()
