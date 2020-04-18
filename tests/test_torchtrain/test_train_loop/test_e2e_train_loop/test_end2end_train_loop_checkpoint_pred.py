8d1bb79ed6211cbcd883ff0b702d362027efe489
Marko Vidoni <marko.vidoni@gmail.com>
2020-04-18 11:00:21 +0200

    Base for test_e2e_ff_net_continue_training_checkpoint_1_back_continue_train_compare_in_memory_end_save

--- tests/test_torchtrain/test_train_loop/test_e2e_train_loop/test_end2end_train_loop_checkpoint_pred.py
+++ tests/test_torchtrain/test_train_loop/test_e2e_train_loop/test_end2end_train_loop_checkpoint_pred.py
@@ -878,6 +878,156 @@ class TestEnd2EndTrainLoopModelOptimizerSaveReloadContinueTraining(unittest.Test
         if os.path.exists(project_path):
             shutil.rmtree(project_path)
 
+    def test_e2e_ff_net_continue_training_checkpoint_1_back_continue_train_compare_in_memory_end_save(self):
+        self.set_seeds()
+        batch_size = 10
+
+        train_dataset = TensorDataset(torch.randn(100, 50), torch.randint(low=0, high=10, size=(100,)))
+        val_dataset = TensorDataset(torch.randn(30, 50), torch.randint(low=0, high=10, size=(30,)))
+        test_dataset = TensorDataset(torch.randn(30, 50), torch.randint(low=0, high=10, size=(30,)))
+
+        train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
+        val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
+        test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
+
+        model = FFNet()
+        optimizer = optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999))
+        criterion = nn.NLLLoss()
+
+        train_loop = TrainLoopCheckpointEndSave(
+            model,
+            train_dataloader, val_dataloader, test_dataloader,
+            optimizer, criterion,
+            project_name='e2e_train_loop_example', experiment_name='TrainLoopCheckpointEndSave_example',
+            local_model_result_folder_path=THIS_DIR,
+            hyperparams={'batch_size': batch_size},
+            val_result_package=ClassificationResultPackage(), test_result_package=ClassificationResultPackage(),
+            cloud_save_mode=None
+        )
+        train_loop.fit(num_epochs=5)
+
+        model_loader_final = PyTorchLocalModelLoader(THIS_DIR)
+        model_loader_final.load_model(train_loop.project_name, train_loop.experiment_name,
+                                      train_loop.experiment_timestamp,
+                                      model_save_dir='model', epoch_num=None)
+        model_reload_final = FFNet()
+        model_reload_final = model_loader_final.init_model(model_reload_final)
+        optimizer_reload_final = optim.Adam(model_reload_final.parameters(), lr=0.001, betas=(0.9, 0.999))
+        optimizer_reload_final = model_loader_final.init_optimizer(optimizer_reload_final)
+        criterion_reload_final = nn.NLLLoss()
+
+        model_loader_ep4 = PyTorchLocalModelLoader(THIS_DIR)
+        model_loader_ep4.load_model(train_loop.project_name, train_loop.experiment_name,
+                                    train_loop.experiment_timestamp,
+                                    model_save_dir='checkpoint_model', epoch_num=3)
+        model_reload_ep4 = FFNet()
+        model_reload_ep4 = model_loader_ep4.init_model(model_reload_ep4)
+        optimizer_reload_ep4 = optim.Adam(model_reload_ep4.parameters(), lr=0.001, betas=(0.9, 0.999))
+        optimizer_reload_ep4 = model_loader_ep4.init_optimizer(optimizer_reload_ep4)
+        criterion_reload_ep4 = nn.NLLLoss()
+
+        for (orig_k, orig_state), (reload_k, reload_state) in zip(model.state_dict().items(),
+                                                                  model_reload_final.state_dict().items()):
+            self.assertEqual(orig_k, reload_k)
+            self.assertEqual(orig_state.tolist(), reload_state.tolist())
+
+        for orig_state, reload_state in zip(optimizer.state_dict()['state'].values(),
+                                            optimizer_reload_final.state_dict()['state'].values()):
+            self.assertEqual(orig_state['step'], reload_state['step'])
+            self.assertEqual(orig_state['exp_avg'].tolist(), reload_state['exp_avg'].tolist())
+            self.assertEqual(orig_state['exp_avg_sq'].tolist(), reload_state['exp_avg_sq'].tolist())
+
+        for (orig_k, orig_state), (reload_k, reload_state) in zip(model.state_dict().items(),
+                                                                  model_reload_ep4.state_dict().items()):
+            self.assertEqual(orig_k, reload_k)
+            self.assertNotEqual(orig_state.tolist(), reload_state.tolist())
+
+        for orig_state, reload_state in zip(optimizer.state_dict()['state'].values(),
+                                            optimizer_reload_ep4.state_dict()['state'].values()):
+            self.assertNotEqual(orig_state['step'], reload_state['step'])
+            self.assertNotEqual(orig_state['exp_avg'].tolist(), reload_state['exp_avg'].tolist())
+            self.assertNotEqual(orig_state['exp_avg_sq'].tolist(), reload_state['exp_avg_sq'].tolist())
+
+        train_loop_reload_final = TrainLoop(
+            model_reload_final,
+            train_dataloader, val_dataloader, test_dataloader,
+            optimizer_reload_final, criterion_reload_final
+        )
+
+        train_loop_reload_ep4 = TrainLoop(
+            model_reload_ep4,
+            train_dataloader, val_dataloader, test_dataloader,
+            optimizer_reload_ep4, criterion_reload_ep4
+        )
+        train_loop_reload_ep4.epoch = 4
+        train_loop_reload_ep4.fit(num_epochs=5)
+
+        for (orig_k, orig_state), (reload_k, reload_state) in zip(model.state_dict().items(),
+                                                                  model_reload_ep4.state_dict().items()):
+            self.assertEqual(orig_k, reload_k)
+            self.assertEqual(orig_state.tolist(), reload_state.tolist())
+
+        for orig_state, reload_state in zip(optimizer.state_dict()['state'].values(),
+                                            optimizer_reload_ep4.state_dict()['state'].values()):
+            self.assertEqual(orig_state['step'], reload_state['step'])
+            self.assertEqual(orig_state['exp_avg'].tolist(), reload_state['exp_avg'].tolist())
+            self.assertEqual(orig_state['exp_avg_sq'].tolist(), reload_state['exp_avg_sq'].tolist())
+
+        for (orig_k, orig_state), (reload_k, reload_state) in zip(model_reload_final.state_dict().items(),
+                                                                  model_reload_ep4.state_dict().items()):
+            self.assertEqual(orig_k, reload_k)
+            self.assertEqual(orig_state.tolist(), reload_state.tolist())
+
+        for orig_state, reload_state in zip(optimizer_reload_final.state_dict()['state'].values(),
+                                            optimizer_reload_ep4.state_dict()['state'].values()):
+            self.assertEqual(orig_state['step'], reload_state['step'])
+            self.assertEqual(orig_state['exp_avg'].tolist(), reload_state['exp_avg'].tolist())
+            self.assertEqual(orig_state['exp_avg_sq'].tolist(), reload_state['exp_avg_sq'].tolist())
+
+        train_pred, _, _ = train_loop.predict_on_train_set()
+        val_pred, _, _ = train_loop.predict_on_validation_set()
+        test_pred, _, _ = train_loop.predict_on_test_set()
+
+        train_pred_reload_final, _, _ = train_loop_reload_final.predict_on_train_set()
+        val_pred_reload_final, _, _ = train_loop_reload_final.predict_on_validation_set()
+        test_pred_reload_final, _, _ = train_loop_reload_final.predict_on_test_set()
+
+        train_pred_reload_ep4, _, _ = train_loop_reload_ep4.predict_on_train_set()
+        val_pred_reload_ep4, _, _ = train_loop_reload_ep4.predict_on_validation_set()
+        test_pred_reload_ep4, _, _ = train_loop_reload_ep4.predict_on_test_set()
+
+        self.assertEqual(train_pred.tolist(), train_pred_reload_final.tolist())
+        self.assertEqual(val_pred.tolist(), val_pred_reload_final.tolist())
+        self.assertEqual(test_pred.tolist(), test_pred_reload_final.tolist())
+
+        self.assertEqual(train_pred.tolist(), train_pred_reload_ep4.tolist())
+        self.assertEqual(val_pred.tolist(), val_pred_reload_ep4.tolist())
+        self.assertEqual(test_pred.tolist(), test_pred_reload_ep4.tolist())
+
+        train_loss = train_loop.evaluate_loss_on_train_set()
+        val_loss = train_loop.evaluate_loss_on_validation_set()
+        test_loss = train_loop.evaluate_loss_on_test_set()
+
+        train_loss_reload_final = train_loop_reload_final.evaluate_loss_on_train_set()
+        val_loss_reload_final = train_loop_reload_final.evaluate_loss_on_validation_set()
+        test_loss_reload_final = train_loop_reload_final.evaluate_loss_on_test_set()
+
+        train_loss_reload_ep4 = train_loop_reload_ep4.evaluate_loss_on_train_set()
+        val_loss_reload_ep4 = train_loop_reload_ep4.evaluate_loss_on_validation_set()
+        test_loss_reload_ep4 = train_loop_reload_ep4.evaluate_loss_on_test_set()
+
+        self.assertEqual(train_loss, train_loss_reload_final)
+        self.assertEqual(val_loss, val_loss_reload_final)
+        self.assertEqual(test_loss, test_loss_reload_final)
+
+        self.assertEqual(train_loss, train_loss_reload_ep4)
+        self.assertEqual(val_loss, val_loss_reload_ep4)
+        self.assertEqual(test_loss, test_loss_reload_ep4)
+
+        project_path = os.path.join(THIS_DIR, 'e2e_train_loop_example')
+        if os.path.exists(project_path):
+            shutil.rmtree(project_path)
+
     @staticmethod
     def set_seeds():
         manual_seed = 0
