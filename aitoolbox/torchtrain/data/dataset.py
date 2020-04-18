a3c79132fbcf4d351b60cb82db757ab1b9f99476
Marko Vidoni <marko.vidoni@gmail.com>
2020-04-18 20:52:31 +0200

    Implemented ListDataset

--- aitoolbox/torchtrain/data/dataset.py
+++ aitoolbox/torchtrain/data/dataset.py
@@ -23,3 +23,29 @@ class BasicDataset(Dataset):
 
         """
         return self.data[idx]
+
+
+class ListDataset(Dataset):
+    def __init__(self, *data_lists):
+        """Dataset wrapping lists.
+
+        Each sample will be retrieved by indexing tensors along the first dimension.
+
+        Examples:
+            list_dataset_1 = [...]
+            list_dataset_2 = [...]
+            list_dataset_3 = [...]
+            ListDataset(list_dataset_1, list_dataset_2, list_dataset_3)
+
+        Arguments:
+            *data_lists (list): data lists that have the same size of the first dimension. Input is represented as
+                a list of data lists.
+        """
+        assert all(len(data_lists[0]) == data_l for data_l in data_lists)
+        self.data_lists = data_lists
+
+    def __getitem__(self, index):
+        return tuple(data_l[index] for data_l in self.data_lists)
+
+    def __len__(self):
+        return len(self.data_lists[0])
