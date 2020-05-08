d57b307100644daaf9050d39d2400e6ca9cd37f9
Marko Vidoni <marko.vidoni@gmail.com>
2020-05-08 20:38:21 +0200

    Package version bump script

---
+++ bump_pkg_version.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+current_version=1.0.2
+bump_type="patch"
+rebuild_pkg=true
+
+bump2version --current-version ${current_version} ${bump_type} setup.py
+bump2version --current-version ${current_version} ${bump_type} install_package.sh
+bump2version --current-version ${current_version} ${bump_type} bin/AWS/create_instance.sh
+bump2version --current-version ${current_version} ${bump_type} bin/AWS/prepare_instance.sh
+bump2version --current-version ${current_version} ${bump_type} bin/AWS/submit_job.sh
+bump2version --current-version ${current_version} ${bump_type} bin/AWS/update_package_on_AWS.sh
+
+
+if [ $uninstallPrevious == true ]; then
+    mv dist/* dist_old
+    ./build_package.sh
+fi
