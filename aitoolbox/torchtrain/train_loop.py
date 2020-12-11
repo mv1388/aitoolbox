7aa78fd357dc93b69b6d570b65026ff643830352
Marko Vidoni <marko.vidoni@gmail.com>
2020-12-11 22:25:28 +0100

    0-based indexing of the total training iterations

--- aitoolbox/torchtrain/train_loop.py
+++ aitoolbox/torchtrain/train_loop.py
@@ -124,7 +124,8 @@ class TrainLoop:
         self.experiment_timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H-%M-%S')
         self.loss_batch_accum = []
         self.epoch = 0
-        self.total_iterations = 0
+        # Intentionally set to -1 because we do += 1 at the start of every iteration
+        self.total_iteration_idx = -1
 
         self.train_history = TrainingHistory(has_validation=self.validation_loader is not None)
         self.prediction_store = ModelPredictionStore(auto_purge=True)
@@ -234,6 +235,7 @@ class TrainLoop:
             self.callbacks_handler.execute_epoch_begin()
 
             for iteration, batch_data in enumerate(tqdm(self.train_loader)):
+                self.total_iteration_idx += 1
                 self.callbacks_handler.execute_batch_begin()
 
                 # Feed batch into the model
@@ -256,8 +258,7 @@ class TrainLoop:
 
                 self.callbacks_handler.execute_batch_end()
 
-                self.total_iterations += 1
-                if self.total_iterations == num_iterations:
+                if self.total_iteration_idx + 1 == num_iterations:
                     break
 
             # Automatic end of epoch code - reports the train and if available validation loss and executes callbacks
@@ -274,7 +275,9 @@ class TrainLoop:
             if self.early_stop:
                 break
 
-            if self.total_iterations == num_iterations:
+            if self.total_iteration_idx + 1 == num_iterations:
+                print(f'Stopping training after {num_iterations} training iterations. '
+                      f'Current iteration index: {self.total_iteration_idx}')
                 break
 
         self.auto_execute_end_of_training()
