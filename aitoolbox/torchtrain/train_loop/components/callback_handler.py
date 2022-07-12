a6ffea6b3eb23f094145ea3094daca9d329e66dc
Marko Vidoni <marko.vidoni@gmail.com>
2022-07-12 12:32:26 +0200

    Merge and move all the functionality into the single CallbacksHandler

--- aitoolbox/torchtrain/train_loop/components/callback_handler.py
+++ aitoolbox/torchtrain/train_loop/components/callback_handler.py
@@ -164,7 +164,7 @@ class BasicCallbacksHandler:
         return False
 
 
-class CallbacksHandler(BasicCallbacksHandler):
+class CallbacksHandler:
     def __init__(self, train_loop_obj):
         """Callback handler used for the callback orchestration inside the TrainLoop
 
@@ -178,7 +178,8 @@ class CallbacksHandler(BasicCallbacksHandler):
         Args:
             train_loop_obj (aitoolbox.torchtrain.train_loop.TrainLoop): reference to the encapsulating TrainLoop
         """
-        super().__init__(train_loop_obj)
+        self.train_loop_obj = train_loop_obj
+        self.callbacks_cache = []
 
         self.cbs_on_epoch_begin = []
         self.cbs_on_epoch_end = []
@@ -201,8 +202,8 @@ class CallbacksHandler(BasicCallbacksHandler):
     def register_callbacks(self, callbacks, cache_callbacks=False):
         """Register TrainLoop object reference inside the listed callbacks when the TrainLoop is created
 
-        Normally, this is called from inside of the train loop by the TrainLoop itself. Basically train loop "registers"
-        itself.
+        Normally, this is called from inside the train loop by the TrainLoop itself. Basically train loop "registers"
+        itself with each of the provided callbacks.
 
         Args:
             callbacks (list or None): list of callbacks
@@ -215,14 +216,26 @@ class CallbacksHandler(BasicCallbacksHandler):
         """
         if cache_callbacks:
             # Just filling the self.callbacks_cache list with callbacks
-            super().register_callbacks(callbacks, cache_callbacks=cache_callbacks)
+            self.callbacks_cache += callbacks if callbacks is not None else []
         else:
-            # hack since self.callbacks_cache is emptied by the super().register_callbacks()
-            callbacks_cache = self.callbacks_cache
-            super().register_callbacks(callbacks, cache_callbacks=cache_callbacks)
-            # Retrieve cached callbacks from self.callbacks_cache and combine with current callbacks
-            callbacks = callbacks_cache + (callbacks if callbacks is not None else [])
+            # Combine any previously cached callbacks with new callbacks
+            # If there aren't any callbacks cached then the callback cache is just an empty list
+            callbacks = self.callbacks_cache + (callbacks if callbacks is not None else [])
+            # Empty the callbacks cache
             self.callbacks_cache = []
+
+            if callbacks is not None and len(callbacks) > 0:
+                self.enforce_callbacks_quality(callbacks)
+                self.train_loop_obj.callbacks += [
+                    cb.register_train_loop_object(self.train_loop_obj) for cb in callbacks
+                    if self.train_loop_obj.device.index is None or
+                       cb.device_idx_execution is None or
+                       (cb.device_idx_execution is not None and cb.device_idx_execution == self.train_loop_obj.device.index)
+                ]
+
+            if not all(0 == cb.execution_order for cb in self.train_loop_obj.callbacks):
+                self.train_loop_obj.callbacks = sorted(self.train_loop_obj.callbacks, key=lambda cb: cb.execution_order)
+
             self.split_on_execution_position(callbacks, register_train_loop=False)
 
     def execute_epoch_begin(self):
@@ -304,7 +317,7 @@ class CallbacksHandler(BasicCallbacksHandler):
                 cbs_at_position.sort(key=lambda cb: cb.execution_order)
                 
     def mp_filter_callbacks(self):
-        super().mp_filter_callbacks()
+        self.mp_filter_callbacks()
         self.cbs_on_epoch_begin = self._mp_filter_cb_list(self.cbs_on_epoch_begin)
         self.cbs_on_epoch_end = self._mp_filter_cb_list(self.cbs_on_epoch_end)
         self.cbs_on_train_begin = self._mp_filter_cb_list(self.cbs_on_train_begin)
@@ -323,6 +336,22 @@ class CallbacksHandler(BasicCallbacksHandler):
             self.cbs_on_multiprocess_start
         ]
 
+    def _mp_filter_cb_list(self, callbacks_list):
+        return [cb for cb in callbacks_list
+                if cb.device_idx_execution is None or
+                (cb.device_idx_execution is not None and cb.device_idx_execution == self.train_loop_obj.device.index)]
+
+    def enforce_callbacks_quality(self, callbacks):
+        for cb in callbacks:
+            if not isinstance(cb, AbstractCallback):
+                raise TypeError(f'Callback {cb} is not inherited from the AbstractCallback')
+
+            if cb.device_idx_execution is not None and self.train_loop_obj.device.index is not None:
+                if cb.device_idx_execution >= torch.cuda.device_count():
+                    raise ValueError(f'Selected device_idx_execution of {cb.device_idx_execution} is too high. '
+                                     f'There are only {torch.cuda.device_count()} available GPU devices. '
+                                     f'Select index ranging from 0 to {torch.cuda.device_count() - 1}')
+
     def __str__(self):
         return 'CALLBACKS\n' \
                f'At on_epoch_begin:\n{self.print_callback_info(self.cbs_on_epoch_begin)}\n' \
@@ -334,3 +363,57 @@ class CallbacksHandler(BasicCallbacksHandler):
                f'At on_after_gradient_update:\n{self.print_callback_info(self.cbs_on_after_gradient_update)}\n' \
                f'At on_after_optimizer_step:\n{self.print_callback_info(self.cbs_on_after_optimizer_step)}\n' \
                f'At on_multiprocess_start:\n{self.print_callback_info(self.cbs_on_multiprocess_start)}'
+
+    @staticmethod
+    def print_callback_info(callback_list):
+        return '\n'.join([f'\t{callback.callback_name}: {type(callback)}, execution_order: {callback.execution_order}'
+                          for callback in callback_list])
+
+    def print_registered_callback_names(self):
+        print(self)
+
+    def __len__(self):
+        return len(self.train_loop_obj.callbacks)
+
+    def __add__(self, other):
+        """
+
+        Args:
+            other (list): callbacks list
+
+        Returns:
+            BasicCallbacksHandler:
+        """
+        self.register_callbacks(other)
+        return self
+
+    def __iadd__(self, other):
+        """
+
+        Args:
+            other (list): callbacks list
+
+        Returns:
+            BasicCallbacksHandler:
+        """
+        self.register_callbacks(other)
+        return self
+
+    def __contains__(self, item):
+        """
+
+        Args:
+            item:
+
+        Returns:
+            bool:
+        """
+        if type(item) == str:
+            for cb in self.train_loop_obj.callbacks:
+                if cb.callback_name == item:
+                    return True
+        else:
+            for cb in self.train_loop_obj.callbacks:
+                if type(cb) == item:
+                    return True
+        return False
