099ae26479712dda3700b70f68a49724c1e8afc1
Marko Vidoni <marko.vidoni@gmail.com>
2022-08-09 19:54:30 +0200

    E2e unittest

---
+++ tests/ddp_cpu_prediction_saver.py
@@ -0,0 +1,31 @@
+import pickle
+from aitoolbox.torchtrain.callbacks.abstract import AbstractCallback
+
+
+class DDPCPUPredictionSave(AbstractCallback):
+    def __init__(self, dir_path, file_name):
+        """Save predictions to pickle file for testing DDP
+
+        Args:
+            dir_path (str): folder path
+            file_name (str): pickle results save file name
+        """
+        super().__init__('DDP prediction saver')
+        self.dir_path = dir_path
+        self.file_name = file_name
+
+    def on_train_end(self):
+        print('Making predictions from the callback')
+        train_loss_aitb = self.train_loop_obj.evaluate_loss_on_train_set(force_prediction=True)
+        val_loss_aitb = self.train_loop_obj.evaluate_loss_on_validation_set(force_prediction=True)
+        test_loss_aitb = self.train_loop_obj.evaluate_loss_on_test_set(force_prediction=True)
+
+        train_pred_aitb, _, _ = self.train_loop_obj.predict_on_train_set(force_prediction=True)
+        val_pred_aitb, _, _ = self.train_loop_obj.predict_on_validation_set(force_prediction=True)
+        test_pred_aitb, _, _ = self.train_loop_obj.predict_on_test_set(force_prediction=True)
+
+        with open(f'{self.dir_path}/{self.file_name}', 'wb') as f:
+            pickle.dump([
+                train_pred_aitb.tolist(), val_pred_aitb.tolist(), test_pred_aitb.tolist(),
+                train_loss_aitb, val_loss_aitb, test_loss_aitb
+            ], f)
--- tests/test_torchtrain/test_train_loop/test_train_loop_pytorch_compare/test_rand_generated_data_ff_net.py
+++ tests/test_torchtrain/test_train_loop/test_train_loop_pytorch_compare/test_rand_generated_data_ff_net.py
@@ -1,6 +1,9 @@
 import unittest
 
+import os
 import random
+import shutil
+import pickle
 import numpy as np
 import torch
 import torch.nn as nn
@@ -9,9 +12,18 @@ import torch.optim as optim
 from torch.utils.data.dataloader import DataLoader
 from torch.utils.data.dataset import TensorDataset
 
+import torch.multiprocessing as mp
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel
+from torch.utils.data.distributed import DistributedSampler
+
 from aitoolbox.torchtrain.train_loop import TrainLoop
 from aitoolbox.torchtrain.model import TTModel
 from aitoolbox.torchtrain.schedulers.warmup import LinearWithWarmupScheduler
+
+from tests.ddp_cpu_prediction_saver import DDPCPUPredictionSave
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 class FFNetAIToolbox(TTModel):
@@ -421,6 +433,221 @@ class TestTrainLoopVSCorePyTorch(unittest.TestCase):
         self.assertEqual(train_loss_aitb, train_loss_pt)
         self.assertEqual(val_loss_aitb, val_loss_pt)
         self.assertEqual(test_loss_aitb, test_loss_pt)
+
+    @staticmethod
+    def set_seeds():
+        manual_seed = 0
+        np.random.seed(manual_seed)
+        random.seed(manual_seed)
+        torch.manual_seed(manual_seed)
+        # if you are suing GPU
+        torch.cuda.manual_seed(manual_seed)
+        torch.cuda.manual_seed_all(manual_seed)
+
+        torch.backends.cudnn.enabled = False
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+
+
+class TestDDPCPUTrainLoopVSCorePyTorch(unittest.TestCase):
+    def test_ddp_cpu_trainloop_core_pytorch_prediction_loss_compare(self):
+        os.mkdir(f'{THIS_DIR}/ddp_temp_save')
+
+        batch_size = 50
+        num_epochs = 10
+
+        train_dataset = TensorDataset(torch.randn(1000, 50), torch.randint(low=0, high=10, size=(1000,)))
+        val_dataset = TensorDataset(torch.randn(300, 50), torch.randint(low=0, high=10, size=(300,)))
+        test_dataset = TensorDataset(torch.randn(300, 50), torch.randint(low=0, high=10, size=(300,)))
+        train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
+        val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
+        test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
+
+        train_pred_aitb, val_pred_aitb, test_pred_aitb, train_loss_aitb, val_loss_aitb, test_loss_aitb = \
+            self.train_eval_trainloop(train_dataloader, val_dataloader, test_dataloader, num_epochs)
+
+        train_pred_pt, val_pred_pt, test_pred_pt, train_loss_pt, val_loss_pt, test_loss_pt = \
+            self.train_eval_core_pytorch(train_dataloader, val_dataloader, test_dataloader, num_epochs)
+
+        print('Doing comparison test')
+
+        self.assertEqual(train_pred_aitb, train_pred_pt)
+        self.assertEqual(val_pred_aitb, val_pred_pt)
+        self.assertEqual(test_pred_aitb, test_pred_pt)
+
+        self.assertEqual(train_loss_aitb, train_loss_pt)
+        self.assertEqual(val_loss_aitb, val_loss_pt)
+        self.assertEqual(test_loss_aitb, test_loss_pt)
+
+        project_path = os.path.join(THIS_DIR, 'ddp_temp_save')
+        if os.path.exists(project_path):
+            shutil.rmtree(project_path)
+
+    def train_eval_trainloop(self, train_dataloader, val_dataloader, test_dataloader, num_epochs=10):
+        self.set_seeds()
+
+        model_aitb = FFNetAIToolbox()
+        optimizer_aitb = optim.Adam(model_aitb.parameters(), lr=0.001, betas=(0.9, 0.999))
+        criterion_aitb = nn.NLLLoss()
+
+        train_loop = TrainLoop(
+            model_aitb,
+            train_dataloader, val_dataloader, test_dataloader,
+            optimizer_aitb, criterion_aitb,
+            gpu_mode='ddp'
+        )
+        train_loop.fit(
+            num_epochs=num_epochs,
+            backend='gloo', num_gpus=2, on_gpu=False,
+            callbacks=[
+                DDPCPUPredictionSave(dir_path=f'{THIS_DIR}/ddp_temp_save',
+                                     file_name='tl_ddp_predictions.p')
+            ]
+        )
+
+        with open(f'{THIS_DIR}/ddp_temp_save/tl_ddp_predictions.p', 'rb') as f:
+            train_pred_aitb, val_pred_aitb, test_pred_aitb, train_loss_aitb, val_loss_aitb, test_loss_aitb = \
+                pickle.load(f)
+
+        return train_pred_aitb, val_pred_aitb, test_pred_aitb, train_loss_aitb, val_loss_aitb, test_loss_aitb
+
+    def train_eval_core_pytorch(self, train_dataloader, val_dataloader, test_dataloader, num_epochs=10):
+        self.set_seeds()
+
+        nprocs = 2
+
+        model_pt = FFNetPyTorch()
+        optimizer_pt = optim.Adam(model_pt.parameters(), lr=0.001, betas=(0.9, 0.999))
+        criterion_pt = nn.NLLLoss()
+
+        os.environ['MASTER_ADDR'] = 'localhost'
+        os.environ['MASTER_PORT'] = '8888'
+        # Based on:
+        # https://blog.exxactcorp.com/pytorch-1-5-1-bug-fix-release/
+        # https://github.com/pytorch/pytorch/issues/37377
+        os.environ['MKL_THREADING_LAYER'] = 'GNU'
+
+        print('Starting the manual DDP training')
+
+        mp.spawn(
+            self.manual_ddp_training,
+            args=(nprocs, num_epochs, model_pt, optimizer_pt, criterion_pt, train_dataloader, val_dataloader, test_dataloader),
+            nprocs=nprocs
+        )
+
+        train_pred_pt, val_pred_pt, test_pred_pt, \
+            train_loss_pt, val_loss_pt, test_loss_pt = [], [], [], [], [], []
+
+        for idx in range(nprocs):
+            with open(f'{THIS_DIR}/ddp_temp_save/pt_ddp_predictions_{idx}.p', 'rb') as f:
+                train_pred_f, val_pred_f, test_pred_f, train_loss_f, val_loss_f, test_loss_f = pickle.load(f)
+                train_pred_pt += train_pred_f
+                val_pred_pt += val_pred_f
+                test_pred_pt += test_pred_f
+                train_loss_pt += train_loss_f
+                val_loss_pt += val_loss_f
+                test_loss_pt += test_loss_f
+
+        train_loss_pt = np.mean(train_loss_pt)
+        val_loss_pt = np.mean(val_loss_pt)
+        test_loss_pt = np.mean(test_loss_pt)
+
+        return train_pred_pt, val_pred_pt, test_pred_pt, train_loss_pt, val_loss_pt, test_loss_pt
+
+    @staticmethod
+    def manual_ddp_training(
+            gpu, nprocs,
+            num_epochs,
+            model_pt, optimizer_pt, criterion_pt,
+            train_loader, val_loader, test_loader
+    ):
+        rank = gpu
+        print(rank)
+        print(f'nprocs: {nprocs}')
+        dist.init_process_group(backend='gloo', init_method='env://', world_size=nprocs, rank=rank)
+        torch.manual_seed(0)
+        device = torch.device('cpu')
+
+        train_sampler = DistributedSampler(dataset=train_loader.dataset, shuffle=True,
+                                           num_replicas=nprocs, rank=rank)
+        val_sampler = DistributedSampler(dataset=val_loader.dataset, shuffle=False,
+                                         num_replicas=nprocs, rank=rank)
+        test_sampler = DistributedSampler(dataset=test_loader.dataset, shuffle=False,
+                                          num_replicas=nprocs, rank=rank)
+        train_loader_ddp = DataLoader(train_loader.dataset, batch_size=50, sampler=train_sampler)
+        val_loader_ddp = DataLoader(val_loader.dataset, batch_size=50, sampler=val_sampler)
+        test_loader_ddp = DataLoader(test_loader.dataset, batch_size=50, sampler=test_sampler)
+
+        model_pt = model_pt.to(device)
+        criterion_pt = criterion_pt.to(device)
+
+        model_pt = DistributedDataParallel(model_pt)
+
+        print('Training')
+        model_pt.train()
+        for epoch in range(num_epochs):
+            print(f'Epoch: {epoch}')
+            train_sampler.set_epoch(epoch)
+
+            for i, (input_data, target) in enumerate(train_loader_ddp):
+                input_data = input_data.to(device)
+                target = target.to(device)
+
+                predicted = model_pt(input_data)
+                loss = criterion_pt(predicted, target)
+                loss.backward()
+                optimizer_pt.step()
+                optimizer_pt.zero_grad()
+
+            # Imitate what happens in auto_execute_end_of_epoch() in TrainLoop
+            for _ in train_loader:
+                pass
+            for _ in val_loader:
+                pass
+
+        train_pred_pt, val_pred_pt, test_pred_pt, train_loss_pt, val_loss_pt, test_loss_pt = [], [], [], [], [], []
+
+        print('Evaluating - train')
+        model_pt.eval()
+        with torch.no_grad():
+            for input_data, target in train_loader_ddp:
+                input_data = input_data.to(device)
+                target = target.to(device)
+
+                predicted = model_pt(input_data)
+                loss_batch = criterion_pt(predicted, target).cpu().item()
+
+                train_pred_pt += predicted.argmax(dim=1, keepdim=False).cpu().tolist()
+                train_loss_pt.append(loss_batch)
+
+        print('Evaluating - validation')
+        model_pt.eval()
+        with torch.no_grad():
+            for input_data, target in val_loader_ddp:
+                input_data = input_data.to(device)
+                target = target.to(device)
+
+                predicted = model_pt(input_data)
+                loss_batch = criterion_pt(predicted, target).cpu().item()
+
+                val_pred_pt += predicted.argmax(dim=1, keepdim=False).cpu().tolist()
+                val_loss_pt.append(loss_batch)
+
+        print('Evaluating - test')
+        model_pt.eval()
+        with torch.no_grad():
+            for input_data, target in test_loader_ddp:
+                input_data = input_data.to(device)
+                target = target.to(device)
+
+                predicted = model_pt(input_data)
+                loss_batch = criterion_pt(predicted, target).cpu().item()
+
+                test_pred_pt += predicted.argmax(dim=1, keepdim=False).cpu().tolist()
+                test_loss_pt.append(loss_batch)
+
+        with open(f'{THIS_DIR}/ddp_temp_save/pt_ddp_predictions_{gpu}.p', 'wb') as f:
+            pickle.dump([train_pred_pt, val_pred_pt, test_pred_pt, train_loss_pt, val_loss_pt, test_loss_pt], f)
 
     @staticmethod
     def set_seeds():
