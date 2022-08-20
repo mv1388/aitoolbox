03df25cbd6e3fc27385a339ab9b45ad41e94f760
Marko Vidoni <marko.vidoni@gmail.com>
2022-08-20 10:01:34 +0200

    Fix the new tests to support DoubleTensor

--- tests_gpu/test_single_gpu/test_amp/test_amp_mnist_cnn.py
+++ tests_gpu/test_single_gpu/test_amp/test_amp_mnist_cnn.py
@@ -435,14 +435,14 @@ class TestAMPGradAccumulationMNISTCNN(unittest.TestCase):
                     predicted = F.log_softmax(x, dim=1)
                     assert predicted.dtype is torch.float32
 
-                    loss_batch = criterion_pt(predicted, target).cpu().item()
+                    loss_batch = criterion_pt(predicted, target).cpu()
 
                     predicted_argmax = predicted.argmax(dim=1, keepdim=False).cpu().tolist()
 
                 val_pred += predicted_argmax
                 val_true += target.cpu().tolist()
                 val_loss.append(loss_batch)
-            val_loss = np.mean(val_loss)
+            val_loss = torch.mean(torch.DoubleTensor(val_loss))
 
         return val_loss, val_pred, val_true
 
