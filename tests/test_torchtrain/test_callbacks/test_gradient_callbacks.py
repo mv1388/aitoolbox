ecef9bc4cb991b4dcd308b0528c43158408701f4
Marko Vidoni <marko.vidoni@gmail.com>
2023-04-04 10:27:12 +0200

    Fix GradientStatsPrint evaluation on every iteration instead of at the end of an epoch

--- tests/test_torchtrain/test_callbacks/test_gradient_callbacks.py
+++ tests/test_torchtrain/test_callbacks/test_gradient_callbacks.py
@@ -11,7 +11,6 @@ import torch.optim as optim
 
 from aitoolbox.torchtrain.callbacks.gradient import GradNormClip, GradientStatsPrint
 from aitoolbox.torchtrain.train_loop import TrainLoop
-from aitoolbox.torchtrain.data.dataset import BasicDataset
 
 
 def build_train_loop(model):
@@ -37,9 +36,9 @@ class TestGradientStatsPrintCallback(unittest.TestCase):
         model = NetUnifiedBatchFeed()
         train_loop = build_train_loop(model)
         train_loop.callbacks_handler.register_callbacks([callback])
-        self.assertFalse(train_loop.grad_cb_used)
+        self.assertTrue(train_loop.grad_cb_used)
 
-        callback = GradientStatsPrint(lambda m: [m.conv1, m.conv2, m.fc1, m.fc2], on_every_grad_update=True)
+        callback = GradientStatsPrint(lambda m: [m.conv1, m.conv2, m.fc1, m.fc2], stats_eval_freq=10)
         model = NetUnifiedBatchFeed()
         train_loop = build_train_loop(model)
         train_loop.callbacks_handler.register_callbacks([callback])
@@ -49,30 +48,35 @@ class TestGradientStatsPrintCallback(unittest.TestCase):
         callback = GradientStatsPrint(lambda m: [m.l1, m.l2])
         model = SmallFFNet()
 
-        x = torch.Tensor(np.random.rand(100, 10))
-        y = torch.Tensor(np.random.rand(100))
+        x = torch.Tensor(np.random.rand(10, 10))
+        y = torch.Tensor(np.random.rand(10))
 
         train_loader = DataLoader(TensorDataset(x, y), batch_size=10)
         optimizer = optim.SGD(model.parameters(), lr=0.01)
         criterion = nn.BCELoss()
+
+        for data_batch in train_loader:
+            batch_loss = model.get_loss(data_batch, criterion, torch.device('cpu'))
+            batch_loss.backward()
+
+        gradients_l1 = model.l1.weight.grad.cpu().numpy()
+        gradients_l2 = model.l2.weight.grad.cpu().numpy()
+        model.zero_grad()
 
         captured_output = io.StringIO()  # Create StringIO object
         sys.stdout = captured_output
 
         TrainLoop(model, train_loader, None, None, optimizer, criterion).fit(num_epochs=1, callbacks=[callback])
         sys.stdout = sys.__stdout__
-        expected_print = '\n'.join(captured_output.getvalue().split('\n')[8:]).strip()
+        cb_output = '\n'.join(captured_output.getvalue().split('\n')[5:-4]).strip()
 
-        gradients_l1 = model.l1.weight.grad.cpu().numpy()
-        gradients_l2 = model.l2.weight.grad.cpu().numpy()
-
-        cb_output = \
+        expected_print = \
             f"""
 ---> Model layers gradients stats
-Layer 0 grads: Mean: {np.mean(gradients_l1)}; Std {np.std(gradients_l1)}
-\tRatio of zero gradients: {float(np.count_nonzero(gradients_l1 == 0)) / gradients_l1.size}
-Layer 1 grads: Mean: {np.mean(gradients_l2)}; Std {np.std(gradients_l2)}
-\tRatio of zero gradients: {float(np.count_nonzero(gradients_l2 == 0)) / gradients_l2.size}
+\tLayer 0 grads: Mean: {np.mean(gradients_l1)}; Std {np.std(gradients_l1)}
+\t\tRatio of zero gradients: {float(np.count_nonzero(gradients_l1 == 0)) / gradients_l1.size}
+\tLayer 1 grads: Mean: {np.mean(gradients_l2)}; Std {np.std(gradients_l2)}
+\t\tRatio of zero gradients: {float(np.count_nonzero(gradients_l2 == 0)) / gradients_l2.size}
         """.strip()
 
         self.assertEqual(expected_print, cb_output)
